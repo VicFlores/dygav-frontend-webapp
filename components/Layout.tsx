ccce73ddefe6bf74288921c7e072df9aa00b2a6e
VicFlores11 <vicflores2211@gmail.com>
2023-08-11 07:45:12 -0600

    feat: [ADD] semantic and styles layout

---
+++ components/Layout.tsx
@@ -0,0 +1,23 @@
+import Head from 'next/head';
+import React, { FC, ReactNode } from 'react';
+import { BurgerMenu, NavBar } from './';
+
+interface Props {
+  children?: ReactNode;
+  title?: string;
+}
+
+export const Layout: FC<Props> = ({ children, title = 'OpenJira App' }) => {
+  return (
+    <div>
+      <Head>
+        <title>{`${title}`}</title>
+      </Head>
+
+      <NavBar />
+      <BurgerMenu />
+
+      <div>{children}</div>
+    </div>
+  );
+};
