6ff420952e0246fa315a34d85078991e5caa27dc
VicFlores11 <vicflores2211@gmail.com>
2023-08-12 17:40:50 -0600

    feat: [ADD] implementation of brand content component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { Layout, Hero, Carousel, AboutUs, OurComments } from '@/components';
+import { BrandContent } from '../components/BrandContent';
 
 const imageUrl =
   'https://res.cloudinary.com/vicflores11/image/upload/v1691367753/Dygav/12_yxhpq1.png';
@@ -7,7 +8,7 @@ export default function Home() {
   return (
     <section>
       <div
-        className={'w-full	 bg-center bg-cover bg-no-repeat'}
+        className={'w-full bg-center bg-cover bg-no-repeat'}
         style={{ backgroundImage: `url(${imageUrl})` }}
       >
         <Layout title='DyGav Home' />
@@ -19,6 +20,8 @@ export default function Home() {
       <AboutUs />
 
       <OurComments />
+
+      <BrandContent />
     </section>
   );
 }
