a8c8f636bd1e85b6eed2fe140293cf8b60a2629f
VicFlores11 <vicflores2211@gmail.com>
2023-08-12 10:11:51 -0600

    feat: [ADD] semantic and styles functionality

---
+++ components/OurComments.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { OurCommentsCard } from './';
+
+export const OurComments = () => {
+  return (
+    <section className='w-full h-auto grid mt-[40px] mb-[40px]'>
+      <div className='self-center justify-self-center'>
+        <p className='text-[34px] text-center  text-black700'>
+          LO QUE DICEN DE NOSOTROS
+        </p>
+        <p className='text-[31.25px] text-center text-black900 pt-1'>
+          Las esperiencias de nuestros propietarios
+        </p>
+      </div>
+
+      <OurCommentsCard />
+    </section>
+  );
+};
