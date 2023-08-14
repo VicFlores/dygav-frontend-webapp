c97e4bb34fc171ea90816241bfa1e653d943fd95
VicFlores11 <vicflores2211@gmail.com>
2023-08-14 08:58:31 -0600

    refactor: [CHANGE] semantic and styles in input search

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,42 +17,42 @@ export const Hero = () => {
 
       <form className='grid lg:grid-cols-4 gap-4 p-[30px] mt-16 bg-p600/50'>
         <div className='grid items-center justify-center relative'>
-          <span className='absolute inset-y-0 left-0 flex items-center pl-2'>
-            <ExtensionIcon sx={{ fontSize: '20px' }} />
-          </span>
-          <input
-            type='text'
-            placeholder='Ubicacion'
-            className='pl-10 bg-transparent shadow appearance-none border border-white placeholder:text-white rounded w-full py-2 text-white leading-tight focus:outline-none focus:shadow-outline'
-          />
+          <label className='relative'>
+            <ExtensionIcon className='w-5 h-5 absolute top-1/2 -translate-y-1/2 left-3' />
+            <input
+              type='text'
+              placeholder='Ubicacion'
+              className='pl-10 bg-transparent shadow appearance-none border border-white placeholder:text-white rounded w-full py-2 text-white leading-tight focus:outline-none focus:shadow-outline'
+            />
+          </label>
         </div>
 
         <div className='grid items-center justify-center relative'>
-          <span className='absolute inset-y-0 left-0 flex items-center pl-10'>
-            <ExtensionIcon sx={{ fontSize: '20px' }} />
-          </span>
-          <input
-            type='date'
-            className='pl-10 pr-4 bg-transparent shadow appearance-none border place border-white placeholder:text-white rounded w-full py-2 text-white leading-tight focus:outline-none focus:shadow-outline'
-          />
+          <label className='relative'>
+            <ExtensionIcon className='w-5 h-5 absolute top-1/2 -translate-y-1/2 left-3' />
+            <input
+              type='date'
+              className='pl-10 pr-4 bg-transparent shadow appearance-none border border-white placeholder:text-white rounded w-full py-2 text-white leading-tight focus:outline-none focus:shadow-outline'
+            />
+          </label>
         </div>
 
         <div className='grid items-center justify-center relative'>
-          <span className='absolute inset-y-0 left-0 flex items-center pl-2'>
-            <ExtensionIcon sx={{ fontSize: '20px' }} />
-          </span>
-          <input
-            type='number'
-            placeholder='Numero de huespedes'
-            className='pl-10 pr-4 bg-transparent shadow appearance-none border border-white placeholder:text-white rounded w-full py-2 text-white leading-tight focus:outline-none focus:shadow-outline'
-          />
+          <label className='relative'>
+            <ExtensionIcon className='w-5 h-5 absolute top-1/2 -translate-y-1/2 left-3' />
+            <input
+              type='number'
+              placeholder='Huespedes'
+              className='pl-10 pr-4 bg-transparent shadow appearance-none border border-white placeholder:text-white rounded w-full py-2 text-white leading-tight focus:outline-none focus:shadow-outline'
+            />
+          </label>
         </div>
 
-        <div className='grid'>
+        <div className='grid justify-center items-center'>
           <Button
             type='submit'
             endIcon={<CheckCircleIcon />}
-            className='bg-p600 hover:bg-p800 py-2 px-4 w-[180px] self-center justify-self-center text-white'
+            className='bg-p600 hover:bg-p800 py-2 px-4 w-[180px] text-white'
           >
             Buscar ahora
           </Button>
