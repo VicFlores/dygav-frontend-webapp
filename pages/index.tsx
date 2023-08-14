941cf7a50fe1b786c27a7c8963de9c4ab5007a54
VicFlores11 <vicflores2211@gmail.com>
2023-08-14 09:05:50 -0600

    refactor: [CHANGE] components import and implement background image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,16 @@
-import { Layout, Hero, Carousel, AboutUs, OurComments } from '@/components';
-import { BrandContent } from '../components/BrandContent';
+import {
+  Layout,
+  Hero,
+  Carousel,
+  AboutUs,
+  OurComments,
+  BrandContent,
+} from '@/components';
 
-const imageUrl =
-  'https://res.cloudinary.com/vicflores11/image/upload/v1691367753/Dygav/12_yxhpq1.png';
-
-export default function Home() {
+export default function HomePage() {
   return (
     <section>
-      <div
-        className={'w-full bg-center bg-cover bg-no-repeat'}
-        style={{ backgroundImage: `url(${imageUrl})` }}
-      >
+      <div className={'w-full bg-heroContent bg-center bg-cover bg-no-repeat '}>
         <Layout title='DyGav Home' />
         <Hero />
       </div>
