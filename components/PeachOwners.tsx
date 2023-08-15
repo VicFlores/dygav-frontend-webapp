533adbe7fbc217d19a25e401aa70400078e0c213
VicFlores11 <vicflores2211@gmail.com>
2023-08-15 11:05:49 -0600

    feat: [ADD] semantic and style in component

---
+++ components/PeachOwners.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+export const PeachOwners = () => {
+  return (
+    <section className='grid justify-center items-center gap-4 mt-24 mb-24'>
+      <p className='text-center text-[34px] text-black700'>
+        ¡Descubre Nuestros servicios de <br /> Gestión de Apartamentos <br />{' '}
+        Vacacionales!
+      </p>
+
+      <div className='w-[1031px] border-t-4 border-t-p600 pt-5 space-y-4 text-black900 pr-3 pl-3'>
+        <p>
+          ¿Tu casa de vacaciones se encuentra vacía durante largos periodos? ¡Es
+          hora de sacarle partido económico!
+        </p>
+        <p>
+          ¡Deja que tu casa de vacaciones trabaje para ti! Rentabiliza tu
+          propiedad mientras no la utilizas y genera ingresos adicionales.
+        </p>
+        <p>
+          Nos encargamos de todos los aspectos necesarios para maximizar tus
+          ingresos y garantizar una experiencia inigualable tanto para los
+          propietarios como para los huéspedes.
+        </p>
+        <p>
+          Nuestro equipo altamente capacitado se encargará de la gestión
+          integral de tus apartamentos, desde la promoción y comercialización en
+          todas las plataformas líderes de gestión de reservas para apartamentos
+          vacacionales (Airbnb, Booking, Rentalia, Vrbo, Google y Expedia),
+          hasta la atención al cliente y el mantenimiento de las propiedades.
+        </p>
+        <p>
+          Nos aseguraremos de que tus apartamentos estén siempre listos, así,
+          tus huéspedes podrán disfrutar de una estancia sin contratiempos y tú
+          podrás obtener las mejores calificaciones y comentarios positivos.
+        </p>
+      </div>
+    </section>
+  );
+};
