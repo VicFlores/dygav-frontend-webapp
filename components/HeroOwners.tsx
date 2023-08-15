98816035f0eba10c763f47518a32cf387c042f3d
VicFlores11 <vicflores2211@gmail.com>
2023-08-15 11:05:22 -0600

    feat: [ADD] semantic and style in component

---
+++ components/HeroOwners.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const HeroOwners = () => {
+  return (
+    <div className='text-white px-[66px] py-[70px]'>
+      <p className='lg:text-[45px] md:text-[40px] pb-5'>
+        Gestión Integral de Apartamentos Turísticos
+      </p>
+      <p className='lg:text-[27px] md:text-[22px]'>
+        En DYGAV nos especializamos en brindar soluciones <br /> integrales para
+        la gestión de apartamentos turísticos.
+      </p>
+    </div>
+  );
+};
