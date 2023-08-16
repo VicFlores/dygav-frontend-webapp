ca8deb6f1f85e48d4c9ad71d9b80a7ec9695249e
VicFlores11 <vicflores2211@gmail.com>
2023-08-16 16:49:10 -0600

    feat: [ADD] semantic and styles props

---
+++ components/Licenses/HeroLicense.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const HeroLicense = () => {
+  return (
+    <section className='text-white px-[66px] py-[70px]'>
+      <p className='lg:text-[45px] md:text-[40px] pb-5 font-bold'>
+        Licencias Turísticas
+      </p>
+      <p className='lg:text-[27px] md:text-[22px]'>
+        Tú Licencia Turística Más Cerca de lo que Crees
+      </p>
+    </section>
+  );
+};
