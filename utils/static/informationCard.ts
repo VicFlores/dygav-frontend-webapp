97bfa6ebf86e1470e8a4815afdc958c0508593f8
VicFlores11 <vicflores2211@gmail.com>
2023-08-16 10:19:08 -0600

    feat: [ADD] information card

---
+++ utils/static/informationCard.ts
@@ -0,0 +1,79 @@
+import { TImageProps } from '@/types';
+
+type TCardProps = TImageProps & {
+  title: string;
+  subtitle: string;
+};
+
+export const informationCard: TCardProps[] = [
+  {
+    src: 'https://res.cloudinary.com/vicflores11/image/upload/v1692135591/Dygav/undraw_Business_plan_re_0v81_pwrgvq.png',
+    alt: 'Inversiones y Mejoras',
+    title: 'Inversiones y Mejoras',
+    subtitle:
+      'Enfocados en aumentar tu rentabilidad, te ofrecemos asesoramiento personalizado sobre inversiones y mejoras clave en tus apartamentos. Ya sea la instalación de tecnología de vanguardia, como sistemas domóticos que optimizan el consumo energético, o la creación de espacios más funcionales, estamos aquí para potenciar el valor de tus propiedades y garantizar un mayor retorno de tu inversión. Además te proporcionaremos informes detallados sobre el desempeño de tus apartamentos y te asesoraremos sobre posibles oportunidades de crecimiento.',
+  },
+  {
+    src: 'https://res.cloudinary.com/vicflores11/image/upload/v1692136610/Dygav/undraw_product_photography_91i2_heu55s.png',
+    alt: 'Reportaje Fotográfico Profesional',
+    title: 'Reportaje Fotográfico Profesional',
+    subtitle:
+      'Nuestro servicio de reportaje fotográfico profesional resaltará la belleza y el encanto de tu casa, generando un mayor interés y aumentando tus reservas. Cada rincón de tu casa será cuidadosamente fotografiado, desde los espacios interiores hasta los exteriores, pasando por los rincones más encantadores. Nos aseguramos de capturar la atmósfera y la personalidad única de tu propiedad.',
+  },
+  {
+    src: 'https://res.cloudinary.com/vicflores11/image/upload/v1692136734/Dygav/undraw_Social_thinking_re_y8cc_bgut8y.png',
+    alt: 'Marketing Y Promoción',
+    title: 'Marketing Y Promoción',
+    subtitle:
+      'Utilizamos estrategias efectivas de marketing digital para aumentar la visibilidad de tus apartamentos y atraer a más huéspedes. Nos encargamos de la creación y actualización de perfiles en TODAS las plataformas de alquiler vacacional, así como de la gestión de las reservas y la optimización de tarifas.',
+  },
+  {
+    src: 'https://res.cloudinary.com/vicflores11/image/upload/v1692136905/Dygav/undraw_Savings_re_eq4w_x3aeo0.png',
+    alt: 'Precios Dinámicos',
+    title: 'Precios Dinámicos',
+    subtitle:
+      'Descubre el poder de la digitalización y maximiza tus ingresos con nuestro revolucionario programa de precios dinámicos. Aprovechamos la demanda en tiempo real para ajustar estratégicamente tus tarifas y garantizarte la máxima rentabilidad.',
+  },
+  {
+    src: 'https://res.cloudinary.com/vicflores11/image/upload/v1692200444/Dygav/undraw_Online_calendar_re_wk3t_deptkr.png',
+    alt: 'Check In / Check Out',
+    title: 'Check In / Check Out',
+    subtitle:
+      'Dile adiós a las preocupaciones relacionadas con el check-in y check-out al proporcionar un sistema sin contacto y totalmente digitalizado. Eliminamos las demoras y complicaciones, permitiéndoles disfrutar de su estancia desde el primer momento y hasta el día de su partida.',
+  },
+  {
+    src: 'https://res.cloudinary.com/vicflores11/image/upload/v1692201915/Dygav/undraw_voice_control_ofo1_qw5akv.png',
+    alt: 'Atención al Cliente',
+    title: 'Atención al Cliente',
+    subtitle:
+      'Nuestro equipo de atención al cliente se encargará de brindar un servicio excepcional a tus huéspedes desde el momento de la reserva hasta su partida. Estaremos disponibles para responder a sus consultas, proporcionar información local y asegurarnos de que tengan una estancia cómoda y satisfactoria. Además tenemos un teléfono para emergencias las 24 horas del día.',
+  },
+  {
+    src: 'https://res.cloudinary.com/vicflores11/image/upload/v1692200706/Dygav/undraw_Clean_up_re_504g_knants.png',
+    alt: 'Limpieza y Mantenimiento',
+    title: 'Limpieza y Mantenimiento',
+    subtitle:
+      'Nos ocupamos de la limpieza profesional de tus apartamentos antes y después de cada estancia, asegurando un alto nivel de higiene y presentación. También coordinamos cualquier tipo de mantenimiento necesario para garantizar que tus propiedades estén en perfectas condiciones.',
+  },
+  {
+    src: 'https://res.cloudinary.com/vicflores11/image/upload/v1692200843/Dygav/undraw_two_factor_authentication_namy_ks1ozk.png',
+    alt: 'Gestión de Seguros',
+    title: 'Gestión de Seguros',
+    subtitle:
+      'No te compliques con los trámites de seguros. Como parte de nuestros servicios, nos encargamos de gestionarlos por ti. Gestionamos los seguros en Airbnb, Booking y otras plataformas, y además, te ayudamos a conseguir los mejores precios en el seguro de tu hogar y en el seguro de responsabilidad civil, garantizando una protección completa y adaptada a tus necesidades. Descansa tranquilo sabiendo que estamos cuidando de tu inversión en todos los aspectos.',
+  },
+  {
+    src: 'https://res.cloudinary.com/vicflores11/image/upload/v1692201132/Dygav/undraw_reviews_lp8w_tqqbkq.png',
+    alt: 'Gestión de Reseñas',
+    title: 'Gestión de Reseñas',
+    subtitle:
+      'En DYGAV nos esforzamos por brindar un servicio excepcional y garantizar que tus huéspedes tengan una experiencia memorable. Además, implementamos estrategias para fomentar y solicitar reseñas positivas de tus huéspedes satisfechos. Estas reseñas positivas no solo impulsarán la confianza de futuros huéspedes, sino que también ampliarán aún más la visibilidad de tus apartamentos en las plataformas de alquiler vacacional. Nuestro objetivo es ayudarte a obtener la mayor cantidad posible de reseñas positivas para que tu propiedad destaque y atraiga a más potenciales huéspedes.',
+  },
+  {
+    src: 'https://res.cloudinary.com/vicflores11/image/upload/v1692201606/Dygav/undraw_data_reports_706v_hirslj.png',
+    alt: 'Nuestras Tarifas',
+    title: 'Nuestras Tarifas',
+    subtitle:
+      'Con nuestra tarifa altamente competitiva, obtendrás todos los beneficios de nuestros servicios de gestión integral: marketing y promoción efectiva, atención al cliente excepcional, limpieza y mantenimiento impecables, y una estrategia de gestión de precios dinámicos inteligente. Al contrario que otras empresas que ocultan sus tarifas escondiendo sus comisiones en DYGAV, mantenemos una política de transparencia total en nuestras tarifas porque creemos en una relación donde nuestros ingresos estén directamente vinculados al éxito de nuestros clientes y por eso cobramos una comisión del 15% sobre la facturación de las casas que gestionamos, lo cual incluye todos nuestros servicios profesionales de gestión.',
+  },
+];
