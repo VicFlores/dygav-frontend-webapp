1e86d0b8a2a309bea40008e7bfba0e5c157cd51d
VicFlores11 <vicflores2211@gmail.com>
2023-08-16 16:54:14 -0600

    feat: [ADD] source card props

---
+++ utils/static/licenseTouristCardInfo.ts
@@ -0,0 +1,25 @@
+import { TCardProps } from '@/types';
+
+export const LicenseTouristCardInfo: TCardProps[] = [
+  {
+    src: 'https://res.cloudinary.com/vicflores11/image/upload/v1692224081/Dygav/undraw_secure_files_re_6vdh_ocaelx.png',
+    alt: 'Cumplimiento Normativo',
+    title: 'Cumplimiento Normativo',
+    subtitle:
+      'Te asesoramos sobre los requisitos legales y las normas específicas que debes cumplir en tu área, desde cuestiones de habitabilidad hasta la seguridad y las instalaciones requeridas.',
+  },
+  {
+    src: 'https://res.cloudinary.com/vicflores11/image/upload/v1692224078/Dygav/undraw_My_documents_re_13dc_oy8qvl.png',
+    alt: 'Documentación Adecuada',
+    title: 'Documentación Adecuada',
+    subtitle:
+      'Te ayudamos a recopilar y preparar todos los documentos necesarios para la solicitud de la licencia, asegurando que estén completos y cumplimentados correctamente.',
+  },
+  {
+    src: 'https://res.cloudinary.com/vicflores11/image/upload/v1692224075/Dygav/undraw_Processing_re_tbdu_yuu38n.png',
+    alt: 'Trámites Administrativos',
+    title: 'Trámites Administrativos',
+    subtitle:
+      'Gestionamos los trámites ante el organismo turístico competente, presentando tu solicitud y realizando el seguimiento necesario para asegurar una respuesta positiva.',
+  },
+];
