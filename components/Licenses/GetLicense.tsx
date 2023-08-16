d4c6a6bad057ca72789fd0f2862e030daa2b9b36
VicFlores11 <vicflores2211@gmail.com>
2023-08-16 16:49:24 -0600

    feat: [ADD] semantic and styles props

---
+++ components/Licenses/GetLicense.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+export const GetLicense = () => {
+  return (
+    <section className='grid gap-4 mt-24 mb-16 px-32'>
+      <p className='text-center text-[34px] text-black700'>
+        ¿Cómo obtener licencia turística?
+      </p>
+
+      <div className='w-auto border-t-4 border-t-p600 pt-5 space-y-4 text-black900 pr-3 pl-3 text-lg'>
+        <p>
+          En DYGAV, estamos a tu lado en cada paso del camino hacia la obtención
+          de la licencia turística para tu piso. Nos convertimos en tu aliado y
+          guía, asegurándonos de que el proceso sea fluido y exitoso. Desde el
+          asesoramiento inicial hasta la presentación de la documentación
+          necesaria, nos encargamos de todo el papeleo y daremos seguimiento
+          constante a todos los trámites para que tú puedas concentrarte en lo
+          más importante: preparar tu piso para recibir a huéspedes.
+        </p>
+
+        <p>
+          Nuestro equipo de expertos en licencias turísticas se mantiene
+          actualizado con las regulaciones y requisitos específicos de tu área,
+          garantizando que tu solicitud cumpla con todos los estándares legales
+          y normativas vigentes. En DYGAV, comprendemos la importancia de
+          obtener la licencia turística de manera rápida y eficiente y nos
+          comprometemos a brindarte el apoyo necesario en cada etapa del
+          proceso, asegurando que puedas abrir las puertas de tu piso al mundo
+          del alquiler vacacional sin preocupaciones. Confía en nosotros para
+          que te acompañemos en todo el camino y aproveches al máximo el
+          potencial de tu propiedad. Ya sea por teléfono, correo electrónico,
+          WhatsApp o a través del formulario de contacto nuestro equipo estará
+          encantado de atenderte y responder a todas tus consultas.
+        </p>
+      </div>
+    </section>
+  );
+};
