6f70648870de5ff6e177b9ce238ec027be674d0c
VicFlores11 <vicflores2211@gmail.com>
2023-08-29 09:52:17 -0600

    feat: [ADD] implementation signup route and auth credentials

--- components/Signup/Register.tsx
+++ components/Signup/Register.tsx
@@ -1,8 +1,56 @@
+import { ChangeEvent, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { signIn } from 'next-auth/react';
+import axios, { AxiosError } from 'axios';
 import { BiExtension } from 'react-icons/bi';
 import { AiOutlineCheckCircle } from 'react-icons/ai';
 import { FcGoogle } from 'react-icons/fc';
 
 export const Register = () => {
+  const [error, setError] = useState('');
+  const router = useRouter();
+
+  const [infoState, setInfoState] = useState({
+    fullname: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const values = e.target.value;
+
+    setInfoState({
+      ...infoState,
+      [e.target.name]: values,
+    });
+  };
+
+  const handleRegister = async () => {
+    const { fullname, email, password } = infoState;
+
+    try {
+      const signupResponse = await axios.post('/api/auth/signup/route', {
+        fullname,
+        email,
+        password,
+      });
+
+      const res = await signIn('credentials', {
+        email: signupResponse.data.email,
+        password,
+        redirect: false,
+      });
+
+      if (res?.error) return setError(res.error);
+
+      if (res?.ok) return router.push('/');
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        setError(error.response?.data.message);
+      }
+    }
+  };
+
   return (
     <div className='flex items-center justify-center mt-10 md:mt-8 lg:mt-0'>
       <div className='bg-white px-6 py-4 md:py-6 lg:py-4 space-y-5 w-[350px] md:w-[450px]'>
@@ -20,6 +68,9 @@ export const Register = () => {
             <BiExtension className='w-4 h-4 md:w-5 md:h-5 absolute top-1/2 -translate-y-1/2 left-3 text-black900' />
             <input
               type='text'
+              name='fullname'
+              value={infoState.fullname}
+              onChange={handleChange}
               placeholder='Escribe tu nombre completo'
               className='py-3 pl-10 pr-4 text-xs md:text-sm lg:text-base bg-transparent shadow appearance-none border-r-2 border-r-black900 placeholder:text-black900 w-full text-black900 leading-tight focus:outline-none focus:shadow-outline'
             />
@@ -29,6 +80,9 @@ export const Register = () => {
             <BiExtension className='w-4 h-4 md:w-5 md:h-5 absolute top-1/2 -translate-y-1/2 left-3 text-black900' />
             <input
               type='email'
+              name='email'
+              value={infoState.email}
+              onChange={handleChange}
               placeholder='Escribe tu email'
               className='py-3 pl-10 pr-4 text-xs md:text-sm lg:text-base bg-transparent shadow appearance-none border-r-2 border-r-black900 placeholder:text-black900 w-full text-black900 leading-tight focus:outline-none focus:shadow-outline'
             />
@@ -38,6 +92,9 @@ export const Register = () => {
             <BiExtension className='w-4 h-4 md:w-5 md:h-5 absolute top-1/2 -translate-y-1/2 left-3 text-black900' />
             <input
               type='password'
+              name='password'
+              value={infoState.password}
+              onChange={handleChange}
               placeholder='Escribe tu contraseña'
               className='py-3 pl-10 pr-4 text-xs md:text-sm lg:text-base bg-transparent shadow appearance-none border-r-2 border-r-black900 placeholder:text-black900 w-full text-black900 leading-tight focus:outline-none focus:shadow-outline'
             />
@@ -48,10 +105,11 @@ export const Register = () => {
           <div className='relative'>
             <AiOutlineCheckCircle className='w-5 md:h-5 text-white absolute top-1/2 -translate-y-1/2 right-10 md:right-5 lg:right-7' />
             <button
-              id='toggle'
+              type='submit'
+              onClick={handleRegister}
               className='bg-p600 hover:bg-p800 text-center text-[13px] md:text-sm lg:text-base py-2 px-4 w-[220px] md:w-[240px] lg:w-[280px] text-white justify-self-center self-center'
             >
-              Iniciar sesión
+              Registrarse
             </button>
           </div>
 
@@ -64,6 +122,12 @@ export const Register = () => {
               Iniciar sesión con Google
             </button>
           </div>
+
+          {error && (
+            <div className='text-center'>
+              <p>{error}</p>
+            </div>
+          )}
         </div>
       </div>
     </div>
