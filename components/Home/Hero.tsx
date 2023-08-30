bab2bd69881917934ed0d22d5b0c70b5ac230188
VicFlores11 <vicflores2211@gmail.com>
2023-08-29 20:10:36 -0600

    style: [CHANGE] spacing between inputs

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -16,36 +16,36 @@ export const Hero = () => {
       </div>
 
       <form className='grid lg:grid-cols-4 lg:gap-4 md:gap-8 gap-10 p-[30px] mt-16 bg-p600/50'>
-        <div className='self-center justify-self-center relative'>
+        <div className='self-center justify-self-center'>
           <label className='relative'>
             <BiExtension className='w-5 h-5 absolute top-1/2 -translate-y-1/2 left-3 text-black900' />
             <input
               type='text'
               placeholder='Ubicacion'
-              className='pl-10 lg:w-56 lg:h-11 md:w-96 w-full bg-transparent shadow appearance-none bg-white border border-white placeholder:text-black900 rounded py-2 text-black900 leading-tight focus:outline-none focus:shadow-outline'
+              className='pl-10 lg:w-full lg:h-11 md:w-96 w-full bg-transparent shadow appearance-none bg-white border border-white placeholder:text-black900 py-2 text-black900 leading-tight focus:outline-none focus:shadow-outline'
             />
           </label>
         </div>
 
-        <div className='self-center justify-self-center relative'>
+        <div className='self-center justify-self-center'>
           <label className='relative'>
             <BiExtension className='w-5 h-5 absolute top-1/2 -translate-y-1/2 left-3 text-black900' />
             <input
               type='text'
               placeholder='Fecha'
               onFocus={(e) => (e.target.type = 'date')}
-              className='pl-10 pr-4 lg:w-56 lg:h-11 md:w-96 w-full bg-transparent shadow appearance-none bg-white border border-white placeholder:text-black900 rounded py-2 text-black900 leading-tight focus:outline-none focus:shadow-outline'
+              className='pl-10 pr-4 lg:w-full lg:h-11 md:w-96 w-full bg-transparent shadow appearance-none bg-white border border-white placeholder:text-black900 py-2 text-black900 leading-tight focus:outline-none focus:shadow-outline'
             />
           </label>
         </div>
 
-        <div className='self-center justify-self-center relative'>
+        <div className='self-center justify-self-center'>
           <label className='relative'>
             <BiExtension className='w-5 h-5 absolute top-1/2 -translate-y-1/2 left-3 text-black900' />
             <input
               type='number'
               placeholder='Huespedes'
-              className='pl-10 pr-4 lg:w-56 lg:h-11 md:w-96 w-full bg-transparent shadow appearance-none bg-white border border-white placeholder:text-black900 rounded py-2 text-black900 leading-tight focus:outline-none focus:shadow-outline'
+              className='pl-10 pr-4 lg:w-full lg:h-11 md:w-96 w-full bg-transparent shadow appearance-none bg-white border border-white placeholder:text-black900 py-2 text-black900 leading-tight focus:outline-none focus:shadow-outline'
             />
           </label>
         </div>
