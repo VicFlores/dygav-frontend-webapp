18018faff6e13b3884d04e287f84b06d9ecf250f
VicFlores11 <vicflores2211@gmail.com>
2023-09-05 09:58:59 -0600

    feat: [ADD] navigation routes

---
+++ utils/static/navigation.ts
@@ -0,0 +1,14 @@
+import { TMenuItem } from '@/types';
+
+export const publicMenuItem: TMenuItem[] = [
+  { title: 'Viaja', path: '/' },
+  { title: 'Apartamentos', path: '/' },
+  { title: 'Propietarios', path: '/' },
+  { title: 'Licencias VUT', path: '/' },
+];
+
+export const accountMenuItem: TMenuItem[] = [
+  { title: 'Reservaciones', path: '/' },
+  { title: 'Apartamentos', path: '/' },
+  { title: 'Mi perfil', path: '/' },
+];
