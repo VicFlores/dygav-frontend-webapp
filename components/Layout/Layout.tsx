dcc85955b861b4402401571773b6a89ae38b5797
VicFlores11 <vicflores2211@gmail.com>
2023-09-04 18:59:59 -0600

    feat: [ADD] user session props in navbar

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,20 +1,26 @@
 import Head from 'next/head';
 import React, { FC, ReactNode } from 'react';
 import { BurgerMenu, NavBar } from '..';
+import { Session } from 'next-auth';
 
 interface Props {
   children?: ReactNode;
   title?: string;
+  session?: Session | null;
 }
 
-export const Layout: FC<Props> = ({ children, title = 'OpenJira App' }) => {
+export const Layout: FC<Props> = ({
+  children,
+  session = null,
+  title = 'OpenJira App',
+}) => {
   return (
     <div>
       <Head>
         <title>{`${title}`}</title>
       </Head>
 
-      <NavBar />
+      <NavBar session={session} />
       <BurgerMenu />
 
       <div>{children}</div>
