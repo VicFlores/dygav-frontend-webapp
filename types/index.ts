92aa2c06ee65ff40d0ad329c525ed92d79d78a02
VicFlores11 <vicflores2211@gmail.com>
2023-09-05 10:00:56 -0600

    feat: [ADD] export type session in barrel file

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,4 @@
 export * from './UI/TMenuItem';
 export * from './UI/TImageProps';
 export * from './UI/TCardProps';
-export * from './mongoose/TUserLogin';
-export * from './mongoose/TUser';
+export * from './UI/TSession';
