4641316962a65400d47d43f3602595b6a9decaa4
VicFlores11 <vicflores2211@gmail.com>
2023-09-04 19:03:11 -0600

    refactor: [REMOVE] unused props

--- pages/license/index.tsx
+++ pages/license/index.tsx
@@ -6,13 +6,8 @@ import {
   Layout,
   LicenseTourist,
 } from '@/components';
-import { useSession } from 'next-auth/react';
 
 export default function License() {
-  const { data: session } = useSession();
-
-  console.log('session:', { session });
-
   return (
     <section>
       <div
