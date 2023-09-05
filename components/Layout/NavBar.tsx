d80449f643540c85701cc022e34042148c111a89
VicFlores11 <vicflores2211@gmail.com>
2023-09-04 18:59:01 -0600

    feat: [ADD] dynamic navbar according to session state

--- components/Layout/NavBar.tsx
+++ components/Layout/NavBar.tsx
@@ -1,62 +1,117 @@
 import { TMenuItem } from '@/types';
+import { Session } from 'next-auth';
+import { signOut } from 'next-auth/react';
 import Image from 'next/legacy/image';
 import Link from 'next/link';
-import React from 'react';
+import { useRouter } from 'next/navigation';
+import React, { FC } from 'react';
 
-const menuItem: TMenuItem[] = [
+const publicMenuItem: TMenuItem[] = [
   { title: 'Viaja', path: '/' },
   { title: 'Apartamentos', path: '/' },
   { title: 'Propietarios', path: '/' },
   { title: 'Licencias VUT', path: '/' },
 ];
 
+const accountMenuItem: TMenuItem[] = [
+  { title: 'Reservaciones', path: '/' },
+  { title: 'Apartamentos', path: '/' },
+  { title: 'Mi perfil', path: '/' },
+];
+
 const imageUrl =
   'https://res.cloudinary.com/vicflores11/image/upload/v1691366906/Dygav/DYGAV_lce25q.svg';
 
-export const NavBar = () => {
+interface Props {
+  session: Session | null;
+}
+
+export const NavBar: FC<Props> = ({ session }) => {
+  const router = useRouter();
+
   return (
-    <nav className='w-full h-32 hidden lg:flex p-4 justify-between items-center static'>
-      <div className='h-auto w-auto relative'>
-        <Image
-          src={imageUrl}
-          alt='DyGav Log'
-          width={100}
-          height={100}
-          priority
-        />
-      </div>
+    <>
+      {session?.user ? (
+        <nav className='w-full h-32 hidden lg:flex p-4 justify-between items-center static bg-p600'>
+          <div className='h-auto w-auto relative'>
+            <Image
+              src={imageUrl}
+              alt='DyGav Log'
+              width={100}
+              height={100}
+              priority
+            />
+          </div>
 
-      <div className='space-x-8'>
-        {menuItem.map((item, index) => (
-          <Link
-            key={index}
-            className=' text-[20px] text-white'
-            href={item.path}
-          >
-            {item.title}
-          </Link>
-        ))}
-      </div>
+          <div className='space-x-8'>
+            {accountMenuItem.map((item, index) => (
+              <Link
+                key={index}
+                className=' text-[20px] text-white'
+                href={item.path}
+              >
+                {item.title}
+              </Link>
+            ))}
+          </div>
 
-      <div className='space-x-6'>
-        <Link
-          href='register'
-          passHref
-          id='toggle'
-          className='bg-white text-p600 px-5 py-2'
-        >
-          Crear Cuenta
-        </Link>
+          <div className='space-x-6'>
+            <button
+              className='bg-white text-p600 px-5 py-2'
+              onClick={async () => {
+                await signOut();
+                router.push('/login');
+              }}
+            >
+              Cerrar Sesion
+            </button>
+          </div>
+        </nav>
+      ) : (
+        <nav className='w-full h-32 hidden lg:flex p-4 justify-between items-center static'>
+          <div className='h-auto w-auto relative'>
+            <Image
+              src={imageUrl}
+              alt='DyGav Log'
+              width={100}
+              height={100}
+              priority
+            />
+          </div>
 
-        <Link
-          href='login'
-          passHref
-          id='toggle'
-          className='bg-p600 text-white px-5 py-2'
-        >
-          Iniciar Sesion
-        </Link>
-      </div>
-    </nav>
+          <div className='space-x-8'>
+            {publicMenuItem.map((item, index) => (
+              <Link
+                key={index}
+                className=' text-[20px] text-white'
+                href={item.path}
+              >
+                {item.title}
+              </Link>
+            ))}
+          </div>
+
+          <div className='space-x-6'>
+            <Link
+              href='register'
+              passHref
+              id='toggle'
+              className='bg-white text-p600 px-5 py-2'
+            >
+              Crear Cuenta
+            </Link>
+
+            <Link
+              href='login'
+              passHref
+              id='toggle'
+              className='bg-p600 text-white px-5 py-2'
+            >
+              Iniciar Sesion
+            </Link>
+          </div>
+        </nav>
+      )}
+    </>
   );
 };
