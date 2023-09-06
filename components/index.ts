1378096bad997c4460ddab91ce080789437664c3
VicFlores11 <vicflores2211@gmail.com>
2023-09-06 12:03:47 -0600

    feat: [ADD] reservation component in barrel file

--- components/index.ts
+++ components/index.ts
@@ -21,3 +21,4 @@ export * from './Licenses/HeroLicense';
 export * from './Licenses/GetLicense';
 export * from './Licenses/LicenseTourist';
 export * from './Dashboard/Dashboard';
+export * from './Reservation/Reservation';
