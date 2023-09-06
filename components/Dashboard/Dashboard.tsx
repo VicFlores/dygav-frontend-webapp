326ff9088a76eaa02c399d7f6e8c49a80881f327
VicFlores11 <vicflores2211@gmail.com>
2023-09-06 09:40:06 -0600

    feat: [ADD] reservation exist view

--- components/Dashboard/Dashboard.tsx
+++ components/Dashboard/Dashboard.tsx
@@ -1,30 +1,92 @@
+import Image from 'next/legacy/image';
 import React from 'react';
 import { AiOutlineCheckCircle } from 'react-icons/ai';
 
 export const Dashboard = () => {
+  const reservation = [
+    {
+      id: 1,
+      imgUrl:
+        'https://res.cloudinary.com/vicflores11/image/upload/v1691367762/Dygav/13_qih5tu.png',
+      title: 'Card title',
+      information:
+        'Lorem ipsum dolor sit amet consectetur. Aliquam quisque tortor sit ac. Ante dolor ultrices dapibus ullamcorper condimentum.',
+    },
+    {
+      id: 2,
+      imgUrl:
+        'https://res.cloudinary.com/vicflores11/image/upload/v1691367762/Dygav/13_qih5tu.png',
+      title: 'Card title',
+      information:
+        'Lorem ipsum dolor sit amet consectetur. Aliquam quisque tortor sit ac. Ante dolor ultrices dapibus ullamcorper condimentum.',
+    },
+    {
+      id: 3,
+      imgUrl:
+        'https://res.cloudinary.com/vicflores11/image/upload/v1691367762/Dygav/13_qih5tu.png',
+      title: 'Card title',
+      information:
+        'Lorem ipsum dolor sit amet consectetur. Aliquam quisque tortor sit ac. Ante dolor ultrices dapibus ullamcorper condimentum.',
+    },
+  ];
+
   return (
-    <div className='px-8 space-y-12'>
+    <div className='px-8 space-y-12 mb-24'>
       <p className=' text-black900/[.7] border-b-[1px] mt-10 text-2xl text-center md:text-left md:text-3xl lg:mt-16 lg:text-4xl'>
         Mis Reservaciones
       </p>
 
-      <div className='flex justify-center items-center'>
-        <div className='flex flex-col justify-center items-center space-y-8 border-[1px] border-dashed h-[266px] w-[717px]'>
-          <p className=' text-black900/[.7] lg:text-3xl'>
-            ¡Aún no tienes reservaciones!
-          </p>
-
-          <div className='relative'>
-            <AiOutlineCheckCircle className='w-5 h-5 absolute top-1/2 -translate-y-1/2 right-3 text-white' />
-            <button
-              id='toggle'
-              className='bg-p600 hover:bg-p800 py-2 px-4 w-[250px] text-left text-white justify-self-center self-center'
+      {reservation ? (
+        <div className='grid gap-y-10 md:grid-cols-2 md:gap-y-8 lg:grid-cols-3 '>
+          {reservation.map((item) => (
+            <div
+              className='rounded-xl space-y-4 justify-self-center border-[1px] border-p600 px-5 py-5 bg-gray300/[.2] w-[300px] md:w-[310px]'
+              key={item.id}
             >
-              Realizar una reservación
-            </button>
+              <figure className='text-center'>
+                <Image
+                  src={item.imgUrl}
+                  alt={item.title}
+                  height={176}
+                  width={267}
+                  priority
+                  className='rounded-[20px]'
+                />
+              </figure>
+
+              <div className='space-y-2'>
+                <p className='text-lg md:text-xl'>{item.title}</p>
+                <p className='text-xs md:text-sm'>{item.information}</p>
+              </div>
+
+              <div className='relative'>
+                <AiOutlineCheckCircle className='w-5 md:h-5 text-p600 absolute top-1/2 -translate-y-1/2 right-5 md:right-12 lg:right-10' />
+                <button className='bg-white text-center text-p600 hover:bg-gray300 border-[1px] border-p600 rounded-md text-[13px] md:text-sm lg:text-base py-2 px-4 w-full'>
+                  Ver reservación
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className='flex justify-center items-center'>
+          <div className='flex flex-col justify-center items-center space-y-8 border-[1px] border-dashed h-[266px] w-[717px]'>
+            <p className=' text-black900/[.7] lg:text-3xl'>
+              ¡Aún no tienes reservaciones!
+            </p>
+
+            <div className='relative'>
+              <AiOutlineCheckCircle className='w-5 h-5 absolute top-1/2 -translate-y-1/2 right-3 text-white' />
+              <button
+                id='toggle'
+                className='bg-p600 hover:bg-p800 py-2 px-4 w-[250px] text-left text-white justify-self-center self-center'
+              >
+                Realizar una reservación
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
