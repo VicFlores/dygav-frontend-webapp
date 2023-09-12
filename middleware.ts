729b250d8c354ed5789b0330d31f9bdc216befb1
VicFlores11 <vicflores2211@gmail.com>
2023-09-12 10:16:58 -0600

    feat:[ADD] protect routes according user roles

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,26 @@
-export { default } from 'next-auth/middleware';
+import { withAuth, NextRequestWithAuth } from 'next-auth/middleware';
+import { NextResponse } from 'next/server';
+import { TUserToken } from './types';
+
+export default withAuth(
+  function middleware(req: NextRequestWithAuth) {
+    const { pathname } = req.nextUrl;
+    const { role } = req.nextauth.token?.user as TUserToken;
+
+    if (pathname.startsWith('/private/owner/dashboard') && role !== 'owner') {
+      return NextResponse.rewrite(new URL('/denied', req.url));
+    }
+
+    if (pathname.startsWith('/private/dashboard') && role !== 'tourist') {
+      return NextResponse.rewrite(new URL('/denied', req.url));
+    }
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+  }
+);
 
 export const config = {
   matcher: ['/private/:path*'],
