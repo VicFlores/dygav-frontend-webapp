d559b1012f5f6c6babd9746e085cd47df83091b4
VicFlores11 <vicflores2211@gmail.com>
2023-09-12 19:08:40 -0600

    feat: [REMOVE] user session prop

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,8 +1,6 @@
-import { useSession } from 'next-auth/react';
 import { Login, NavBar } from '@/components';
 
 export default function LoginPage() {
-  const { data: session } = useSession();
   return (
     <section>
       <div
@@ -10,7 +8,7 @@ export default function LoginPage() {
           'w-full bg-brandContent bg-center bg-cover bg-no-repeat h-[calc(100vh+280px)] md:h-screen'
         }
       >
-        <NavBar session={session} />
+        <NavBar />
         <Login />
       </div>
     </section>
--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -1,9 +1,6 @@
-import { useSession } from 'next-auth/react';
 import { NavBar, Register } from '@/components';
 
 export default function RegisterPage() {
-  const { data: session } = useSession();
-
   return (
     <section>
       <div
@@ -11,7 +8,7 @@ export default function RegisterPage() {
           'w-full bg-brandContent bg-center bg-cover bg-no-repeat h-[calc(100vh+320px)] md:h-screen'
         }
       >
-        <NavBar session={session} />
+        <NavBar />
 
         <Register />
       </div>
