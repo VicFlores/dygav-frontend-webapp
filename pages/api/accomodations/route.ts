88d5346351beaec54d53287d756e734b3f194407
VicFlores11 <vicflores2211@gmail.com>
2023-09-13 13:38:48 -0600

    feat: [ADD] accomodation route and method post

---
+++ pages/api/accomodations/route.ts
@@ -0,0 +1,24 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { connectDB } from '@/utils/db/database';
+import { Accomodations } from '@/types';
+import Accomodation from '@/models/accomodations';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'POST') {
+    const body: Accomodations = req.body;
+
+    try {
+      await connectDB();
+
+      const savedAccomodation = await Accomodation.create(body);
+
+      res.status(200).json(savedAccomodation);
+    } catch (error) {
+      if (error instanceof Error)
+        return res.status(400).json({ message: error.message });
+    }
+  } else return res.status(201).json('Cant access');
+}
