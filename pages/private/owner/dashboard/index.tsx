72865d564398f132d71828332bd7efb0bbc7beae
VicFlores11 <vicflores2211@gmail.com>
2023-09-14 19:56:49 -0600

    feat: [ADD] session and accomodation in SSR

--- pages/private/owner/dashboard/index.tsx
+++ pages/private/owner/dashboard/index.tsx
@@ -1,13 +1,39 @@
-import { useSession } from 'next-auth/react';
+import { getSession, useSession } from 'next-auth/react';
 import { Layout, OwnerDashboard } from '@/components';
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import { axiosConfig } from '@/utils';
+import { Accomodations } from '@/types';
+import { Session } from 'next-auth';
 
-export default function OwnerDashboardPage() {
-  const { data: session } = useSession();
+type OwnerDash = {
+  session: Session | null;
+  data: Accomodations[];
+};
 
+export const getServerSideProps: GetServerSideProps<OwnerDash> = async (
+  ctx
+) => {
+  const session = await getSession(ctx);
+
+  const id = session?.user?._id;
+
+  const accomodationByUser = await axiosConfig.get(
+    `/api/accomodations/findByUserId/${id}`
+  );
+
+  const data = accomodationByUser.data;
+
+  return { props: { session, data } };
+};
+
+export default function OwnerDashboardPage({
+  session,
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <section>
       <Layout session={session} title='Dygav Dash'>
-        <OwnerDashboard />
+        <OwnerDashboard data={data} />
       </Layout>
     </section>
   );
