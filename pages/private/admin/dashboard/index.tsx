765f02158a58faf128bb503c83c3b558374f934d
VicFlores11 <vicflores2211@gmail.com>
2023-09-16 13:20:00 -0600

    feat: [REMOVE] session prop in admin dash component

--- pages/private/admin/dashboard/index.tsx
+++ pages/private/admin/dashboard/index.tsx
@@ -1,14 +1,14 @@
-import { getSession, useSession } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
 import { Layout, AdminDashboard } from '@/components';
 
-export default function OwnerDashboardPage() {
+export default function AdminDashboardPage() {
   const { data: session } = useSession();
 
   return (
     <section>
       <Layout session={session} title='Dygav Dash'>
         {session ? (
-          <AdminDashboard session={session} />
+          <AdminDashboard />
         ) : (
           <>
             <h1>Waiting</h1>
