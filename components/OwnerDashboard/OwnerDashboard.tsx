4bd43a9419af6351373f98797ecfb19034adc166
VicFlores11 <vicflores2211@gmail.com>
2023-09-18 16:32:38 -0600

    style: [ADD] button to redirect callender

--- components/OwnerDashboard/OwnerDashboard.tsx
+++ components/OwnerDashboard/OwnerDashboard.tsx
@@ -64,6 +64,17 @@ export const OwnerDashboard: FC<{ session: Session }> = ({ session }) => {
                   Ver Reservas
                 </Link>
               </div>
+
+              <div className='flex justify-center items-center relative'>
+                <AiOutlineCheckCircle className='w-5 md:h-5 text-p600 absolute top-1/2 -translate-y-1/2 right-5 md:right-12 lg:right-10' />
+                <Link
+                  href={`/private/owner/accomodationCallender/${item.accomodationId}`}
+                  passHref
+                  className='text-center text-p600 hover:bg-gray300 border-[1px] border-p600 rounded-md text-[13px] md:text-sm lg:text-base py-2 px-4 w-full'
+                >
+                  Ver Calendario
+                </Link>
+              </div>
             </div>
           ))}
         </div>
