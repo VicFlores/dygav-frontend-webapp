cd7af39746409fd229c9b47801b190582095b2d2
VicFlores11 <vicflores2211@gmail.com>
2023-09-21 15:13:38 -0600

    feat: [ADD] searcher card info in barrel file

--- utils/index.ts
+++ utils/index.ts
@@ -2,3 +2,4 @@ export * from './static/informationCard';
 export * from './static/licenseTouristCardInfo';
 export * from './static/navigation';
 export * from './config/axiosConfig';
+export * from './static/seacherCardInfo';
