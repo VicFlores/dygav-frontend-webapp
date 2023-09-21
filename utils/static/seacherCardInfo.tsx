42a6903e52369c732d1299bd7ec0a6018356f1c0
VicFlores11 <vicflores2211@gmail.com>
2023-09-21 15:13:24 -0600

    feat: [ADD] searcher card info

---
+++ utils/static/seacherCardInfo.tsx
@@ -0,0 +1,98 @@
+import { TImageProps } from '@/types';
+
+type TSearcherCard = TImageProps & {
+  imgSubtitle: string;
+  title: string;
+  subtitle: string;
+  bedroom: number;
+  bathroom: number;
+  size: string;
+  id: number;
+};
+
+export const searcherCard: TSearcherCard[] = [
+  {
+    id: 1,
+    src: 'https://res.cloudinary.com/vicflores11/image/upload/v1691367769/Dygav/5_mhpa2f.png',
+    alt: 'Search Image Card',
+    width: 347,
+    height: 227,
+    imgSubtitle: 'Torrevieja-100€ noche',
+    title: 'Piso céntrico en Torrevieja: Ubicación ideal para tu escapada',
+    subtitle:
+      'Disfruta de una estancia cómoda en nuestro acogedor piso en el centro de Torrevieja. Habitación elegante y comodidades modernas. ¡Reserva ahora y descubre la encantadora ciudad costera!',
+    bedroom: 1,
+    bathroom: 1,
+    size: '90m²',
+  },
+  {
+    id: 2,
+    src: 'https://res.cloudinary.com/vicflores11/image/upload/v1691367752/Dygav/11_uhpghg.png',
+    alt: 'Search Image Card',
+    width: 347,
+    height: 227,
+    imgSubtitle: 'Torrevieja-100€ noche',
+    title: 'Piso céntrico en Torrevieja: Ubicación ideal para tu escapada',
+    subtitle:
+      'Disfruta de una estancia cómoda en nuestro acogedor piso en el centro de Torrevieja. Habitación elegante y comodidades modernas. ¡Reserva ahora y descubre la encantadora ciudad costera!',
+    bedroom: 1,
+    bathroom: 1,
+    size: '90m²',
+  },
+  {
+    id: 3,
+    src: 'https://res.cloudinary.com/vicflores11/image/upload/v1691367753/Dygav/1_j8qeqo.png',
+    alt: 'Search Image Card',
+    width: 347,
+    height: 227,
+    imgSubtitle: 'Torrevieja-100€ noche',
+    title: 'Piso céntrico en Torrevieja: Ubicación ideal para tu escapada',
+    subtitle:
+      'Disfruta de una estancia cómoda en nuestro acogedor piso en el centro de Torrevieja. Habitación elegante y comodidades modernas. ¡Reserva ahora y descubre la encantadora ciudad costera!',
+    bedroom: 1,
+    bathroom: 1,
+    size: '90m²',
+  },
+  {
+    id: 4,
+    src: 'https://res.cloudinary.com/vicflores11/image/upload/v1691367769/Dygav/5_mhpa2f.png',
+    alt: 'Search Image Card',
+    width: 347,
+    height: 227,
+    imgSubtitle: 'Torrevieja-100€ noche',
+    title: 'Piso céntrico en Torrevieja: Ubicación ideal para tu escapada',
+    subtitle:
+      'Disfruta de una estancia cómoda en nuestro acogedor piso en el centro de Torrevieja. Habitación elegante y comodidades modernas. ¡Reserva ahora y descubre la encantadora ciudad costera!',
+    bedroom: 1,
+    bathroom: 1,
+    size: '90m²',
+  },
+  {
+    id: 5,
+    src: 'https://res.cloudinary.com/vicflores11/image/upload/v1691367752/Dygav/11_uhpghg.png',
+    alt: 'Search Image Card',
+    width: 347,
+    height: 227,
+    imgSubtitle: 'Torrevieja-100€ noche',
+    title: 'Piso céntrico en Torrevieja: Ubicación ideal para tu escapada',
+    subtitle:
+      'Disfruta de una estancia cómoda en nuestro acogedor piso en el centro de Torrevieja. Habitación elegante y comodidades modernas. ¡Reserva ahora y descubre la encantadora ciudad costera!',
+    bedroom: 1,
+    bathroom: 1,
+    size: '90m²',
+  },
+  {
+    id: 6,
+    src: 'https://res.cloudinary.com/vicflores11/image/upload/v1691367753/Dygav/1_j8qeqo.png',
+    alt: 'Search Image Card',
+    width: 347,
+    height: 227,
+    imgSubtitle: 'Torrevieja-100€ noche',
+    title: 'Piso céntrico en Torrevieja: Ubicación ideal para tu escapada',
+    subtitle:
+      'Disfruta de una estancia cómoda en nuestro acogedor piso en el centro de Torrevieja. Habitación elegante y comodidades modernas. ¡Reserva ahora y descubre la encantadora ciudad costera!',
+    bedroom: 1,
+    bathroom: 1,
+    size: '90m²',
+  },
+];
