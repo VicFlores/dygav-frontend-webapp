22c9a7e1613a8389a81565b15461ececb8362a9a
Vic Flores <vicflores2211@gmail.com>
2023-10-19 13:05:09 -0600

    style:[CHANGE] text center to mobile view

--- components/Licenses/HeroLicense.tsx
+++ components/Licenses/HeroLicense.tsx
@@ -7,11 +7,11 @@ type Props = {
 
 export const HeroLicense: FC<Props> = ({ title, subtitle }) => {
   return (
-    <section className="text-white px-[66px] pb-48 pt-20 space-y-4">
-      <p className="lg:text-[45px] text-center md:text-start text-[30px] md:text-[38px] font-semibold">
+    <section className='text-white px-[66px] pb-48 pt-20 space-y-4'>
+      <p className='lg:text-[45px] text-center md:text-start text-[30px] md:text-[38px] font-semibold'>
         {title}
       </p>
-      <p className="text-xl lg:text-[27px] md:text-[22px] font-semibold">
+      <p className='text-xl lg:text-[27px] md:text-[22px] font-semibold text-center lg:text-start'>
         {subtitle}
       </p>
     </section>
