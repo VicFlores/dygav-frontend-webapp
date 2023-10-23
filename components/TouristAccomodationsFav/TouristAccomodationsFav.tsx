bd45b959517a69b1d6f0840b736f93937c0c403e
VicFlores11 <vicflores2211@gmail.com>
2023-10-23 16:21:30 -0600

    feat: [ADD] component to favorite accomodations

---
+++ components/TouristAccomodationsFav/TouristAccomodationsFav.tsx
@@ -0,0 +1,21 @@
+import { searcherCard } from '@/utils';
+import React from 'react';
+import { SearcherCards } from '@/components';
+
+export const TouristAccomodationsFav = () => {
+  const limitedSearcherCard = searcherCard.slice(0, 3); // Extract the first 3 items from the array
+
+  return (
+    <div className='px-8 space-y-12 mb-24'>
+      <p className=' text-black900/[.7] border-b-2 mt-10 text-2xl text-center md:text-left md:text-3xl lg:mt-16 lg:text-4xl'>
+        Mis alojamientos favoritos
+      </p>
+
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-32 mb-56 gap-y-20'>
+        {limitedSearcherCard.map((item, index) => (
+          <SearcherCards key={index} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
