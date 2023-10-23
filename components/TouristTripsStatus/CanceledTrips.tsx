417fac16a87ed8d96fe6ab28a8ff37e9cda7d7c3
VicFlores11 <vicflores2211@gmail.com>
2023-10-23 16:21:05 -0600

    feat: [ADD] component according trip status

---
+++ components/TouristTripsStatus/CanceledTrips.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+
+export const CanceledTrips = () => {
+  return (
+    <section>
+      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-6'>
+        <div className='text-center w-fit bg-gray300/70 p-4 rounded-2xl'>
+          <p className='font-semibold'>Nombre del alojamiento:</p>
+          <p>
+            Un paraíso privado en Torrevieja: Casa unifamiliar con piscina,
+            jacuzzi y sauna
+          </p>
+
+          <p className='font-semibold mt-4'>Fecha de entrada:</p>
+          <p>07-05-2023</p>
+
+          <p className='font-semibold mt-4'>Fecha de salida:</p>
+          <p>10-05-2023</p>
+
+          <p className='font-semibold mt-4'>Hora de entrada:</p>
+          <p>2:00 pm</p>
+
+          <p className='font-semibold mt-4'>Hora de salida:</p>
+          <p>12:00 pm</p>
+
+          <p className='font-semibold mt-4'>Estado de pago:</p>
+          <p>Cancelado</p>
+        </div>
+
+        <div className='text-center w-fit bg-gray300/70 p-4 rounded-2xl'>
+          <p className='font-semibold'>Nombre del alojamiento:</p>
+          <p>
+            Un paraíso privado en Torrevieja: Casa unifamiliar con piscina,
+            jacuzzi y sauna
+          </p>
+
+          <p className='font-semibold mt-4'>Fecha de entrada:</p>
+          <p>07-05-2023</p>
+
+          <p className='font-semibold mt-4'>Fecha de salida:</p>
+          <p>10-05-2023</p>
+
+          <p className='font-semibold mt-4'>Hora de entrada:</p>
+          <p>2:00 pm</p>
+
+          <p className='font-semibold mt-4'>Hora de salida:</p>
+          <p>12:00 pm</p>
+
+          <p className='font-semibold mt-4'>Estado de pago:</p>
+          <p>Cancelado</p>
+        </div>
+
+        <div className='text-center w-fit bg-gray300/70 p-4 rounded-2xl'>
+          <p className='font-semibold'>Nombre del alojamiento:</p>
+          <p>
+            Un paraíso privado en Torrevieja: Casa unifamiliar con piscina,
+            jacuzzi y sauna
+          </p>
+
+          <p className='font-semibold mt-4'>Fecha de entrada:</p>
+          <p>07-05-2023</p>
+
+          <p className='font-semibold mt-4'>Fecha de salida:</p>
+          <p>10-05-2023</p>
+
+          <p className='font-semibold mt-4'>Hora de entrada:</p>
+          <p>2:00 pm</p>
+
+          <p className='font-semibold mt-4'>Hora de salida:</p>
+          <p>12:00 pm</p>
+
+          <p className='font-semibold mt-4'>Estado de pago:</p>
+          <p>Cancelado</p>
+        </div>
+      </div>
+    </section>
+  );
+};
---
+++ components/TouristTripsStatus/PastTrips.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+
+export const PastTrips = () => {
+  return (
+    <section>
+      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-6'>
+        <div className='text-center w-fit bg-gray300/70 p-4 rounded-2xl'>
+          <p className='font-semibold'>Nombre del alojamiento:</p>
+          <p>
+            Bungaló junto al Mar: Tu Refugio de Tranquilidad en la Dehesa de
+            Campoamor
+          </p>
+
+          <p className='font-semibold mt-4'>Fecha de entrada:</p>
+          <p>14-03-2023</p>
+
+          <p className='font-semibold mt-4'>Fecha de salida:</p>
+          <p>16-03-2023</p>
+
+          <p className='font-semibold mt-4'>Hora de entrada:</p>
+          <p>2:00 pm</p>
+
+          <p className='font-semibold mt-4'>Hora de salida:</p>
+          <p>12:00 pm</p>
+
+          <p className='font-semibold mt-4'>Estado de pago:</p>
+          <p>Pagado</p>
+        </div>
+
+        <div className='text-center w-fit bg-gray300/70 p-4 rounded-2xl'>
+          <p className='font-semibold'>Nombre del alojamiento:</p>
+          <p>
+            Bungaló junto al Mar: Tu Refugio de Tranquilidad en la Dehesa de
+            Campoamor
+          </p>
+
+          <p className='font-semibold mt-4'>Fecha de entrada:</p>
+          <p>14-03-2023</p>
+
+          <p className='font-semibold mt-4'>Fecha de salida:</p>
+          <p>16-03-2023</p>
+
+          <p className='font-semibold mt-4'>Hora de entrada:</p>
+          <p>2:00 pm</p>
+
+          <p className='font-semibold mt-4'>Hora de salida:</p>
+          <p>12:00 pm</p>
+
+          <p className='font-semibold mt-4'>Estado de pago:</p>
+          <p>Pagado</p>
+        </div>
+
+        <div className='text-center w-fit bg-gray300/70 p-4 rounded-2xl'>
+          <p className='font-semibold'>Nombre del alojamiento:</p>
+          <p>
+            Bungaló junto al Mar: Tu Refugio de Tranquilidad en la Dehesa de
+            Campoamor
+          </p>
+
+          <p className='font-semibold mt-4'>Fecha de entrada:</p>
+          <p>14-03-2023</p>
+
+          <p className='font-semibold mt-4'>Fecha de salida:</p>
+          <p>16-03-2023</p>
+
+          <p className='font-semibold mt-4'>Hora de entrada:</p>
+          <p>2:00 pm</p>
+
+          <p className='font-semibold mt-4'>Hora de salida:</p>
+          <p>12:00 pm</p>
+
+          <p className='font-semibold mt-4'>Estado de pago:</p>
+          <p>Pagado</p>
+        </div>
+      </div>
+    </section>
+  );
+};
