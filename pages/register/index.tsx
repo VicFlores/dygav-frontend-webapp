92e662eb0890e0f11231f97eba611d504dce8877
VicFlores11 <vicflores2211@gmail.com>
2023-10-25 10:59:07 -0600

    style: [CHANGE] h property

--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -1,5 +1,5 @@
-import { Layout, Register, UtilHead } from "@/components";
-import { useSession } from "next-auth/react";
+import { Layout, Register, UtilHead } from '@/components';
+import { useSession } from 'next-auth/react';
 
 export default function RegisterPage() {
   const { data: session } = useSession();
@@ -10,7 +10,7 @@ export default function RegisterPage() {
 
       <div
         className={
-          "w-full bg-brandContent bg-center bg-cover bg-no-repeat h-[calc(100vh+320px)] md:h-auto"
+          'w-full bg-brandContent bg-center bg-cover bg-no-repeat h-auto'
         }
       >
         <Layout>
