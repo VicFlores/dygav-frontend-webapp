ad0a1ba40441f8e5aaff374a08ece4cc0a1957da
Fernando Aguilar <feraguilar6985@gmail.com>
2023-10-31 16:14:23 -0600

    :sparkles:feat(mailing):add mailing function to license contact form

--- components/Licenses/LicenseAditionalInfo.tsx
+++ components/Licenses/LicenseAditionalInfo.tsx
@@ -3,8 +3,37 @@ import Link from "next/link";
 import { GrContactInfo } from "react-icons/gr";
 import { HiOutlineMail, HiOutlineLocationMarker } from "react-icons/hi";
 import { MdOutlineAddIcCall } from "react-icons/md";
+import {useForm} from 'react-hook-form'
+import { useState } from "react";
+
+
+type Inputs = {
+  name: string,
+  email: string,
+  phone: number,
+  description: string
+}
 
 export const LicenseAditionalInfo = () => {
+
+  const {register, handleSubmit, formState: {errors}} = useForm<Inputs>()
+
+  const [message, setMessage] = useState<string>()
+
+  const onSubmit = (data: Inputs) => {
+
+   
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+  };
+    fetch('/api/mailing', requestOptions)
+        .then(response => response.json())
+  
+        setMessage("Mensaje enviado correctamente")
+  }
+
   return (
     <section className='mt-24 mb-16 px-6 md:px-12 lg:px-32'>
       <h1
@@ -148,7 +177,7 @@ export const LicenseAditionalInfo = () => {
         </div>
 
         <form
-          id='formulario'
+          onSubmit={handleSubmit(onSubmit)}
           className='grid gap-8 px-6 lg:px-10 py-6 pt-4 border-2 border-gray300'
         >
           <p className='text-xl text-center'>Completa tu información</p>
@@ -158,8 +187,10 @@ export const LicenseAditionalInfo = () => {
             <input
               type='text'
               placeholder='Escribe tu nombre completo'
+              {...register('name', {required: true})}
               className='py-3 pl-10 pr-4 bg-transparent shadow appearance-none border-r-2 border-r-black900 placeholder:text-black900 w-full text-black900 leading-tight focus:outline-none focus:shadow-outline'
             />
+            {errors.name && <span className='text-red-500'>Este campo es obligatorio</span>}
           </label>
 
           <label className='relative'>
@@ -167,8 +198,10 @@ export const LicenseAditionalInfo = () => {
             <input
               type='email'
               placeholder='Escribe tu correo electronico'
+              {...register('email', {required: true})}
               className='py-3 pl-10 pr-4 bg-transparent shadow appearance-none border-r-2 border-r-black900 placeholder:text-black900 w-full text-black900 leading-tight focus:outline-none focus:shadow-outline'
             />
+            {errors.email && <span className='text-red-500'>Este campo es obligatorio</span>}
           </label>
 
           <label className='relative'>
@@ -176,24 +209,32 @@ export const LicenseAditionalInfo = () => {
             <input
               type='number'
               placeholder='Escribe tu telefono'
+              {...register('phone', {required: true})}
               className='py-3 pl-10 pr-4 bg-transparent shadow appearance-none border-r-2 border-r-black900 placeholder:text-black900 w-full text-black900 leading-tight focus:outline-none focus:shadow-outline'
             />
+            {errors.phone && <span className='text-red-500'>Este campo es obligatorio</span>}
           </label>
 
           <label className='relative'>
             <HiOutlineLocationMarker className='w-5 h-5 absolute top-1/3 -translate-y-1/2 left-3 text-black900' />
             <textarea
               placeholder='Localidad'
+              {...register('description', {required: true})}
               className='py-3 pl-10 pr-4 bg-transparent shadow appearance-none border-r-2 border-r-black900 placeholder:text-black900 w-full text-black900 leading-tight focus:outline-none focus:shadow-outline'
             />
+            {errors.description && <span className='text-red-500'>Este campo es obligatorio</span>}
           </label>
 
           <button
             id='toggle'
+            type="submit"
             className='bg-p600 hover:bg-p800 text-center text-sm md:text-base py-2 px-4 w-[220px] md:w-[240px] lg:w-[280px] text-white justify-self-center self-center'
           >
             Solicitar informacion
           </button>
+          {
+            message && <p className='text-center text-green-500'>{message}</p>
+          }
         </form>
       </div>
     </section>
