3143da391e6c336e5e242f228f42ce8532f1e2cb
vicflores11 <vicflores2211@gmail.com>
2023-11-08 15:16:50 -0600

    style: [CHANGE] h1 to p tag

--- components/Licenses/HeroLicense.tsx
+++ components/Licenses/HeroLicense.tsx
@@ -8,12 +8,12 @@ type Props = {
 export const HeroLicense: FC<Props> = ({ title, subtitle }) => {
   return (
     <section className='text-white px-[66px] pb-48 pt-20 space-y-4'>
-      <h1 className='lg:text-[45px] text-center md:text-start text-[30px] md:text-[38px] font-semibold'>
+      <p className='lg:text-[45px] text-center md:text-start text-[30px] md:text-[38px] font-semibold'>
         {title}
-      </h1>
-      <h3 className='text-xl lg:text-[27px] md:text-[22px] font-semibold text-center lg:text-start'>
+      </p>
+      <p className='text-xl lg:text-[27px] md:text-[22px] font-semibold text-center lg:text-start'>
         {subtitle}
-      </h3>
+      </p>
     </section>
   );
 };
