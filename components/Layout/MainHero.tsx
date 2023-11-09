8c4579f2b2cdff0e47ef6ce8cc146a5511551bf0
vicflores11 <vicflores2211@gmail.com>
2023-11-09 16:19:20 -0600

    refactor: [CHANGE] double quoute to single quoute

--- components/Layout/MainHero.tsx
+++ components/Layout/MainHero.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactNode, useEffect, useState } from "react";
+import React, { FC, ReactNode, useEffect, useState } from 'react';
 
 interface Props {
   children?: ReactNode;
@@ -8,14 +8,14 @@ interface Props {
 export const MainHero: FC<Props> = ({
   children,
   images = [
-    "https://res.cloudinary.com/vicflores11/image/upload/v1691367769/Dygav/4_wq4pgw.WebP",
-    "https://res.cloudinary.com/vicflores11/image/upload/v1691367769/Dygav/5_mhpa2f.WebP",
-    "https://res.cloudinary.com/vicflores11/image/upload/v1691367762/Dygav/13_qih5tu.WebP",
-    "https://res.cloudinary.com/vicflores11/image/upload/v1691367754/Dygav/20_zc0zeu.WebP",
-    "https://res.cloudinary.com/vicflores11/image/upload/v1691367753/Dygav/1_j8qeqo.WebP",
+    'https://res.cloudinary.com/vicflores11/image/upload/v1691367769/Dygav/4_wq4pgw.WebP',
+    'https://res.cloudinary.com/vicflores11/image/upload/v1691367769/Dygav/5_mhpa2f.WebP',
+    'https://res.cloudinary.com/vicflores11/image/upload/v1691367762/Dygav/13_qih5tu.WebP',
+    'https://res.cloudinary.com/vicflores11/image/upload/v1691367754/Dygav/20_zc0zeu.WebP',
+    'https://res.cloudinary.com/vicflores11/image/upload/v1691367753/Dygav/1_j8qeqo.WebP',
   ],
 }) => {
-  const [backgroundImage, setBackgroundImage] = useState("");
+  const [backgroundImage, setBackgroundImage] = useState('');
 
   useEffect(() => {
     const randomIndex = Math.floor(Math.random() * images.length);
@@ -24,7 +24,7 @@ export const MainHero: FC<Props> = ({
 
   return (
     <div
-      className={"w-full bg-center bg-cover bg-no-repeat"}
+      className={'w-full bg-center bg-cover bg-no-repeat'}
       style={{
         backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.5)), url(${backgroundImage})`,
       }}
