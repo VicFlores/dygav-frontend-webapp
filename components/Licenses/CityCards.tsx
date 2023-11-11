ca4adfb1dd87c8cefc02e785b4c139d14f779cd0
vicflores11 <vicflores2211@gmail.com>
2023-11-10 18:18:49 -0600

    style:[CHANGE] cards width

--- components/Licenses/CityCards.tsx
+++ components/Licenses/CityCards.tsx
@@ -16,7 +16,7 @@ export const CityCards: FC<Props> = ({ images }) => {
     <div className='w-auto h-auto items-center mt-6 p-6 overflow-x-auto overscroll-x-contain flex space-x-6 overflow-y-hidden lg:col-start-2 lg:col-end-4'>
       {images.map((image, index) => (
         <Link href={`${image.href}`} key={index}>
-          <div className=' w-80 h-96 shadow-md rounded-lg overflow-hidden flex-none transform transition-all hover:-translate-y-4 hover:shadow-xl'>
+          <div className='w-60 h-80 md:w-80 md:h-96 shadow-md rounded-lg overflow-hidden flex-none transform transition-all hover:-translate-y-4 hover:shadow-xl'>
             <div
               className={`w-full h-full bg-cover bg-center flex items-end ${image.bgCity}`}
             >
