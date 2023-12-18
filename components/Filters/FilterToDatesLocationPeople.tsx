0060cf845d390633b6c003ec78e53914e9ee160e
vicflores11 <vicflores2211@gmail.com>
2023-12-18 11:48:39 -0600

    feat:[CHANGE] input type and add label title

--- components/Filters/FilterToDatesLocationPeople.tsx
+++ components/Filters/FilterToDatesLocationPeople.tsx
@@ -46,9 +46,10 @@ export const FilterToDatesLocationPeople = () => {
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
-      className='grid lg:grid-cols-5 lg:gap-4 md:gap-8 gap-10 p-[30px] mt-16 bg-p600/50'
+      className='grid lg:grid-cols-5 lg:gap-0 md:gap-8 gap-10 p-[30px] mt-16 bg-p600/50'
     >
-      <div className='self-center justify-self-center'>
+      <div className='text-center self-center justify-self-center'>
+        <p className='pb-2'>Escoge tu cuidad</p>
         <label className='relative'>
           <FaLocationDot className='w-5 h-5 absolute top-1/2 -translate-y-1/2 left-3 text-black900' />
           <select
@@ -69,47 +70,46 @@ export const FilterToDatesLocationPeople = () => {
         </label>
       </div>
 
-      <div className='self-center justify-self-center'>
+      <div className='text-center self-center justify-self-center'>
+        <p className='pb-2'>Escoge tu check-in</p>
         <label className='relative'>
           <BsCalendar2DateFill className='w-5 h-5 absolute top-1/2 -translate-y-1/2 left-3 text-black900' />
           <input
             {...register('checkin', { required: true })}
-            type='text'
-            onFocus={(e) => (e.currentTarget.type = 'date')}
-            onBlur={(e) => (e.currentTarget.type = 'text')}
+            type='date'
             placeholder='Check-in'
-            className='pl-10 pr-4 lg:w-full lg:h-11 md:w-96 w-full bg-transparent shadow appearance-none bg-white border border-white placeholder:text-black900 py-2 text-black900 leading-tight focus:outline-none focus:shadow-outline'
+            className='pl-10 pr-4 lg:w-full lg:h-11 md:w-96 w-52 bg-transparent shadow appearance-none bg-white border border-white placeholder:text-black900 py-2 text-black900 leading-tight focus:outline-none focus:shadow-outline'
           />
         </label>
       </div>
 
-      <div className='self-center justify-self-center'>
+      <div className='text-center self-center justify-self-center'>
+        <p className='pb-2'>Escoge tu check-out</p>
         <label className='relative'>
           <BsCalendar2DateFill className='w-5 h-5 absolute top-1/2 -translate-y-1/2 left-3 text-black900' />
           <input
             {...register('checkout', { required: true })}
-            type='text'
-            onFocus={(e) => (e.currentTarget.type = 'date')}
-            onBlur={(e) => (e.currentTarget.type = 'text')}
+            type='date'
             placeholder='Check-out'
-            className='pl-10 pr-4 lg:w-full lg:h-11 md:w-96 w-full bg-transparent shadow appearance-none bg-white border border-white placeholder:text-black900 py-2 text-black900 leading-tight focus:outline-none focus:shadow-outline'
+            className='pl-10 pr-4 lg:w-full lg:h-11 md:w-96 w-52 bg-transparent shadow appearance-none bg-white border border-white placeholder:text-black900 py-2 text-black900 leading-tight focus:outline-none focus:shadow-outline'
           />
         </label>
       </div>
 
-      <div className='self-center justify-self-center'>
+      <div className='text-center self-center justify-self-center'>
+        <p className='pb-2'>Cantidad de personas</p>
         <label className='relative'>
           <BsPeopleFill className='w-5 h-5 absolute top-1/2 -translate-y-1/2 left-3 text-black900' />
           <input
             {...register('huespedes', { required: true })}
             type='number'
             placeholder='Huéspedes'
-            className='pl-10 pr-4 lg:w-full lg:h-11 md:w-96 w-full bg-transparent shadow appearance-none bg-white border border-white placeholder:text-black900 py-2 text-black900 leading-tight focus:outline-none focus:shadow-outline'
+            className='pl-10 pr-4 lg:w-48 lg:h-11 md:w-96 w-full bg-transparent shadow appearance-none bg-white border border-white placeholder:text-black900 py-2 text-black900 leading-tight focus:outline-none focus:shadow-outline'
           />
         </label>
       </div>
 
-      <div className='self-center justify-self-center relative'>
+      <div className='self-end justify-self-center relative'>
         <div className='relative'>
           <AiOutlineCheckCircle className='w-5 h-5 absolute top-1/2 -translate-y-1/2 right-7' />
           <button
