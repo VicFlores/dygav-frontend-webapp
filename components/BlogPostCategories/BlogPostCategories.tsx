bfdf54819562a6ffaebfd0be73295bfa9e2c3cb5
vicflores11 <vicflores2211@gmail.com>
2023-12-30 09:22:40 -0600

    feat:[ADD] missing categories

--- components/BlogPostCategories/BlogPostCategories.tsx
+++ components/BlogPostCategories/BlogPostCategories.tsx
@@ -12,48 +12,64 @@ export const BlogPostCategories: FC<{
   posts: BlogPostWithSubCategory[];
   query: ParsedUrlQuery;
   filterByCategories: Category[];
-}> = ({ posts, query, filterByCategories }) => (
-  <section className='mt-16 px-6 md:px-12 lg:px-28 flex flex-col items-center'>
-    <CategorySection
-      title='Destinos turísticos'
-      subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.'
-      posts={posts}
-    />
+}> = ({ posts, query, filterByCategories }) => {
+  return (
+    <section className='mt-16 px-6 md:px-12 lg:px-28 flex flex-col items-center'>
+      <CategorySection
+        title='Destinos turísticos'
+        subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua.'
+        posts={posts}
+      />
 
-    <CategorySection
-      title='Gastronomía Local'
-      subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.'
-      posts={posts}
-    />
+      <CategorySection
+        title='Gastronomía Local'
+        subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua.'
+        posts={posts}
+      />
 
-    <CategorySection
-      title='Gestión de Alquileres Vacacionales'
-      subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.'
-      posts={posts}
-    />
+      <CategorySection
+        title='Gestión de Alquileres Vacacionales'
+        subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua.'
+        posts={posts}
+      />
 
-    <CategorySection
-      title='Guías de viaje'
-      subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.'
-      posts={posts}
-    />
+      <CategorySection
+        title='Guías de viaje'
+        subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua.'
+        posts={posts}
+      />
 
-    <CategorySection
-      title='Ocio y Entretenimiento'
-      subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.'
-      posts={posts}
-    />
+      <CategorySection
+        title='Ocio y Entretenimiento'
+        subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua.'
+        posts={posts}
+      />
 
-    <PostCardCategories
-      filterByCategories={filterByCategories}
-      query={query}
-      title='Más artículos sobre otros municipios'
-      subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-    />
-  </section>
-);
+      <CategorySection
+        title='Trámites para licencias turísticas'
+        subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua.'
+        posts={posts}
+      />
+
+      <CategorySection
+        title='Tendencias sobre alquiler vacacional'
+        subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua.'
+        posts={posts}
+      />
+
+      <PostCardCategories
+        filterByCategories={filterByCategories}
+        query={query}
+        title='Más artículos sobre otros municipios'
+        subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      />
+    </section>
+  );
+};
--- components/BlogPostCategories/CategorySection.tsx
+++ components/BlogPostCategories/CategorySection.tsx
@@ -1,5 +1,6 @@
 import { BlogPost } from '@/types';
 import moment from 'moment';
+import Image from 'next/legacy/image';
 import React, { FC, useRef } from 'react';
 import { AiOutlineArrowRight, AiOutlineArrowLeft } from 'react-icons/ai';
 
@@ -32,6 +33,9 @@ export const CategorySection: FC<CategorySectionProps> = ({
     }
   };
 
+  const defaultImage =
+    'https://res.cloudinary.com/vicflores11/image/upload/v1703948850/Dygav/undraw_publish_article_re_3x8h_w41fes.svg';
+
   return (
     <>
       <h2 className='mt-0 md:mt-10 text-2xl md:text-4xl font-semibold text-center'>
@@ -53,26 +57,45 @@ export const CategorySection: FC<CategorySectionProps> = ({
             ref={scrollContainer}
             className='w-auto h-auto items-center pb-8 overflow-x-auto overscroll-x-contain flex space-x-6 overflow-y-hidden scrollbar'
           >
-            {posts
-              .filter((post) => post.subCategoryName === title)
-              .map((post) => (
-                <div key={post.id}>
-                  <div
-                    className='flex flex-col justify-end min-w-[300px] min-h-[350px]  md:min-w-[494px] md:h-[296px] border-2 border-p600 rounded-xl px-6 pb-6 space-y-2 text-white'
-                    style={{
-                      backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.5)), url(${post.yoast_head_json.og_image[0].url})`,
-                      backgroundSize: 'cover',
-                      backgroundPosition: 'center',
-                      boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.25)',
-                    }}
-                  >
-                    <p>{post.title.rendered}</p>
-                    <p>{moment(post.date).format('D MMMM, YYYY')}</p>
+            {posts.filter((post) => post.subCategoryName === title).length >
+            0 ? (
+              posts
+                .filter((post) => post.subCategoryName === title)
+                .map((post) => (
+                  <div key={post.id}>
+                    <div
+                      className='flex flex-col justify-end min-w-[300px] min-h-[350px]  md:min-w-[494px] md:h-[296px] border-2 border-p600 rounded-xl px-6 pb-6 space-y-2 text-white'
+                      style={{
+                        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.5)), url(${post.yoast_head_json.og_image[0].url})`,
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                        boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.25)',
+                      }}
+                    >
+                      <p>{post.title.rendered}</p>
+                      <p>{moment(post.date).format('D MMMM, YYYY')}</p>
+                    </div>
                   </div>
-                </div>
-              ))}
-          </div>
+                ))
+            ) : (
+              <div>
+                <div className='flex flex-col justify-center items-center min-w-[300px] min-h-[350px]  md:min-w-[494px] md:h-[296px] border-2 border-p600 rounded-xl text-p600'>
+                  <figure className='relative w-52 h-52 md:w-60 md:h-60 m-auto'>
+                    <Image
+                      src={
+                        'https://res.cloudinary.com/vicflores11/image/upload/v1703948850/Dygav/undraw_publish_article_re_3x8h_w41fes.svg'
+                      }
+                      layout='fill'
+                    />
+                  </figure>
 
+                  <p className='pb-6 text-center px-4'>
+                    Estamos trabajando para entregarte nuevos articulos
+                  </p>
+                </div>
+              </div>
+            )}
+          </div>
           <button
             onClick={scrollRight}
             className='hidden lg:flex p-2 relative z-10 text-white bg-p600/80 ml-3 -bottom-36'
