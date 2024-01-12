78a86daa59ea504d888621889ec7e5da8b4b6fb8
vicflores11 <vicflores2211@gmail.com>
2024-01-11 19:03:49 -0600

    style: [REMOVE] background and border image icon

--- components/Licenses/LicencesIcons.tsx
+++ components/Licenses/LicencesIcons.tsx
@@ -4,7 +4,7 @@ import React, { FC } from 'react';
 
 export const LicencesIcons: FC<{ image: string }> = ({ image }) => {
   return (
-    <div className='mt-16 mb-10 bg-gray300/40 border border-p600 rounded-xl px-10 py-10 md:py-5'>
+    <div className='mt-16 mb-10 px-10 py-10 md:py-5'>
       <figure className='h-44 w-44 md:h-60 md:w-60 lg:h-56 lg:w-96 relative m-0'>
         <Image
           src={`${image}`}
