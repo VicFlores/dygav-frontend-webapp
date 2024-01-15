30313f1a18dc5b6364312d98a013035be5b7f560
vicflores11 <vicflores2211@gmail.com>
2024-01-15 10:39:19 -0600

    style: [REMOVE] margin bottom

--- components/Licenses/LicenseAditionalInfo.tsx
+++ components/Licenses/LicenseAditionalInfo.tsx
@@ -194,7 +194,7 @@ export const LicenseAditionalInfo = () => {
       </div>
 
       <div
-        className='w-full h-auto grid md:grid-cols-2 lg:grid-cols-3 mt-10 md:mt-10 px-6 md:px-2 lg:px-0 gap-x-4 mb-14'
+        className='w-full h-auto grid md:grid-cols-2 lg:grid-cols-3 mt-10 md:mt-10 px-6 md:px-2 lg:px-0 gap-x-4'
         id='recomendadas'
       >
         <div className='md:self-center md:justify-self-center divide-y-4 divide-p600 text-center md:text-start'>
