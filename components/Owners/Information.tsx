f3bd8d20cb6e216f2c70d647789ce1d8db6f0ab6
vicflores11 <vicflores2211@gmail.com>
2024-01-27 20:44:07 -0600

    refactor:[REMOVE] lorem text

--- components/Owners/Information.tsx
+++ components/Owners/Information.tsx
@@ -296,18 +296,6 @@ export const Information = () => {
         Conoce más sobre la gestión de viviendas turísticas en tu ciudad
       </h2>
 
-      <p className='text-justify lg:text-start text-sm md:text-lg mt-4'>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the standard dummy text ever since the
-        1500s, when an unknown printer took a galley of type and scrambled it to
-        make a type specimen book. It has survived not only five centuries, but
-        also the leap into electronic typesetting, remaining essentially
-        unchanged. It was popularised in the 1960s with the release of Letraset
-        sheets containing Lorem Ipsum passages, and more recently with desktop
-        publishing software like Aldus PageMaker including versions of Lorem
-        Ipsum.
-      </p>
-
       <CityCards
         images={managementHomes}
         cardTitle='Gestion de viviendas en'
