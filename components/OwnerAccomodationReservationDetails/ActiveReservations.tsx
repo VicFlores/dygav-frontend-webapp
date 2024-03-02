03f6c9d3b3ffd23748d1e45cd1101c5f76f459de
VicFlores11 <vicflores2211@gmail.com>
2024-03-02 10:56:13 -0600

    feat:[CHANGE] date format

--- components/OwnerAccomodationReservationDetails/ActiveReservations.tsx
+++ components/OwnerAccomodationReservationDetails/ActiveReservations.tsx
@@ -1,6 +1,6 @@
-import Link from 'next/link';
-import React, { FC, Fragment } from 'react';
-import { AiOutlineCheckCircle } from 'react-icons/ai';
+import Link from "next/link";
+import React, { FC } from "react";
+import { AiOutlineCheckCircle } from "react-icons/ai";
 
 interface IBookingId {
   id: string;
@@ -17,162 +17,129 @@ export const ActiveReservations: FC<{
   bookings: IBookingId[];
   status: string;
 }> = ({ bookings, status }) => {
-  const bookingsCancelled = bookings.filter(
-    (booking) => booking.status === 'CANCELLED'
-  );
+  bookings.sort((a, b) => {
+    if (!a.occupiedPeriod || !b.occupiedPeriod) {
+      return 0;
+    }
+    return (
+      new Date(a.occupiedPeriod.startDate).getTime() -
+      new Date(b.occupiedPeriod.startDate).getTime()
+    );
+  });
 
   return (
     <>
       {bookings.length > 0 ? (
-        <div className='grid gap-y-10 md:grid-cols-2 md:gap-y-8 lg:grid-cols-3'>
-          {status === 'CONFIRMED' ? (
-            bookings.map((booking) =>
-              booking.status === 'CONFIRMED' ? (
-                <div
-                  key={booking.id}
-                  className='text-center h-fit rounded-xl space-y-4 justify-self-center border-[1px] border-p600 px-5 py-5 bg-gray300/[.14] w-[280px] md:w-[300px]'
-                >
-                  <div className='space-y-2'>
-                    <p className='text-base md:text-lg font-semibold'>
-                      Nombre del alojamiento:
-                    </p>
-                    <p className='text-sm md:text-base'>
-                      {booking.accommodationName}
-                    </p>
-
-                    <p className='text-base md:text-lg font-semibold'>
-                      Periodo de reserva:
-                    </p>
-
-                    {booking.occupiedPeriod && booking.occupiedPeriod ? (
-                      <p className='text-sm md:text-base'>
-                        {booking.occupiedPeriod.startDate.toString()} /{' '}
-                        {booking.occupiedPeriod.endDate.toString()}{' '}
-                      </p>
-                    ) : (
-                      <p className='text-sm md:text-base'>
-                        No asignado debido a su cancelacion
-                      </p>
-                    )}
-
-                    <p className='text-base md:text-lg font-semibold'>
-                      Estado:
-                    </p>
-
-                    <p className='text-sm md:text-base'>Confirmado</p>
-                    <p className='text-base md:text-lg font-semibold'>
-                      Mas informacion:
-                    </p>
-                    <div className='flex justify-center bookings-center relative'>
-                      <AiOutlineCheckCircle className='w-5 md:h-5 text-p600 absolute top-1/2 -translate-y-1/2 right-5 md:right-12 lg:right-12' />
-                      <Link
-                        href={`/private/owner/reservation/${booking.id}`}
-                        passHref
-                        className='text-center text-p600 hover:bg-gray300 border-[1px] border-p600 rounded-md text-[13px] md:text-sm lg:text-base py-2 px-4 w-full'
-                      >
-                        Ver detalles
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              ) : booking.status === 'PENDING_PAYMENT' ? (
-                <div
-                  key={booking.id}
-                  className='text-center h-fit rounded-xl space-y-4 justify-self-center border-[1px] border-p600 px-5 py-5 bg-gray300/[.14] w-[280px] md:w-[300px]'
-                >
-                  <div className='space-y-2'>
-                    <p className='text-base md:text-lg font-semibold'>
-                      Nombre del alojamiento:
-                    </p>
-                    <p className='text-sm md:text-base'>
-                      {booking.accommodationName}
-                    </p>
-
-                    <p className='text-base md:text-lg font-semibold'>
-                      Periodo de reserva:
-                    </p>
-
-                    {booking.occupiedPeriod && booking.occupiedPeriod ? (
-                      <p className='text-sm md:text-base'>
-                        {booking.occupiedPeriod.startDate.toString()} /{' '}
-                        {booking.occupiedPeriod.endDate.toString()}{' '}
-                      </p>
-                    ) : (
-                      <p className='text-sm md:text-base'>
-                        No asignado debido a su cancelacion
-                      </p>
-                    )}
-
-                    <p className='text-base md:text-lg font-semibold'>
-                      Estado:
-                    </p>
-
-                    <p className='text-sm md:text-base'>Pago pendiente</p>
-                    <p className='text-base md:text-lg font-semibold'>
-                      Mas informacion:
-                    </p>
-                    <p className='text-sm md:text-base'>
-                      Esperando pago, para ver detalles
-                    </p>
-                  </div>
-                </div>
-              ) : null
-            )
-          ) : status === 'CANCELLED' ? (
-            <>
-              {bookingsCancelled.length > 0 ? (
-                bookings.map((booking) => (
+        <div className="grid gap-y-10 md:grid-cols-2 md:gap-y-8 lg:grid-cols-3">
+          {status === "CONFIRMED"
+            ? bookings.map((booking) =>
+                booking.status === "CONFIRMED" ? (
                   <div
                     key={booking.id}
-                    className='text-center h-fit rounded-xl space-y-4 justify-self-center border-[1px] border-p600 px-5 py-5 bg-gray300/[.14] w-[280px] md:w-[300px]'
+                    className="text-center h-fit rounded-xl space-y-4 justify-self-center border-[1px] border-p600 px-5 py-5 bg-gray300/[.14] w-[280px] md:w-[300px]"
                   >
-                    <div className='space-y-2'>
-                      <p className='text-base md:text-lg font-semibold'>
+                    <div className="space-y-2">
+                      <p className="text-base md:text-lg font-semibold">
                         Nombre del alojamiento:
                       </p>
-                      <p className='text-sm md:text-base'>
+                      <p className="text-sm md:text-base">
                         {booking.accommodationName}
                       </p>
 
-                      <p className='text-base md:text-lg font-semibold'>
+                      <p className="text-base md:text-lg font-semibold">
                         Periodo de reserva:
                       </p>
 
-                      <p className='text-sm md:text-base'>
-                        No asignado debido a su cancelacion
-                      </p>
+                      {booking.occupiedPeriod && booking.occupiedPeriod ? (
+                        <p className="text-sm md:text-base">
+                          {new Date(
+                            booking.occupiedPeriod.startDate
+                          ).toLocaleDateString("en-GB")}{" "}
+                          -{" "}
+                          {new Date(
+                            booking.occupiedPeriod.endDate
+                          ).toLocaleDateString("en-GB")}{" "}
+                        </p>
+                      ) : (
+                        <p className="text-sm md:text-base">
+                          No asignado debido a su cancelacion
+                        </p>
+                      )}
 
-                      <p className='text-base md:text-lg font-semibold'>
+                      <p className="text-base md:text-lg font-semibold">
                         Estado:
                       </p>
 
-                      <p className='text-sm md:text-base'>Cancelado</p>
-                      <p className='text-base md:text-lg font-semibold'>
+                      <p className="text-sm md:text-base">Confirmado</p>
+                      <p className="text-base md:text-lg font-semibold">
                         Mas informacion:
                       </p>
-                      <p className='text-sm md:text-base'>
-                        Reservacion cancelada, para mayor informacion
-                        comuniquese con su administrador
+                      <div className="flex justify-center bookings-center relative">
+                        <AiOutlineCheckCircle className="w-5 md:h-5 text-p600 absolute top-1/2 -translate-y-1/2 right-5 md:right-12 lg:right-12" />
+                        <Link
+                          href={`/private/owner/reservation/${booking.id}`}
+                          passHref
+                          className="text-center text-p600 hover:bg-gray300 border-[1px] border-p600 rounded-md text-[13px] md:text-sm lg:text-base py-2 px-4 w-full"
+                        >
+                          Ver detalles
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                ) : booking.status === "PENDING_PAYMENT" ? (
+                  <div
+                    key={booking.id}
+                    className="text-center h-fit rounded-xl space-y-4 justify-self-center border-[1px] border-p600 px-5 py-5 bg-gray300/[.14] w-[280px] md:w-[300px]"
+                  >
+                    <div className="space-y-2">
+                      <p className="text-base md:text-lg font-semibold">
+                        Nombre del alojamiento:
+                      </p>
+                      <p className="text-sm md:text-base">
+                        {booking.accommodationName}
+                      </p>
+
+                      <p className="text-base md:text-lg font-semibold">
+                        Periodo de reserva:
+                      </p>
+
+                      {booking.occupiedPeriod && booking.occupiedPeriod ? (
+                        <p className="text-sm md:text-base">
+                          {new Date(
+                            booking.occupiedPeriod.startDate
+                          ).toLocaleDateString("en-GB")}{" "}
+                          -{" "}
+                          {new Date(
+                            booking.occupiedPeriod.endDate
+                          ).toLocaleDateString("en-GB")}{" "}
+                        </p>
+                      ) : (
+                        <p className="text-sm md:text-base">
+                          No asignado debido a su cancelacion
+                        </p>
+                      )}
+
+                      <p className="text-base md:text-lg font-semibold">
+                        Estado:
+                      </p>
+
+                      <p className="text-sm md:text-base">Pago pendiente</p>
+                      <p className="text-base md:text-lg font-semibold">
+                        Mas informacion:
+                      </p>
+                      <p className="text-sm md:text-base">
+                        Esperando pago, para ver detalles
                       </p>
                     </div>
                   </div>
-                ))
-              ) : (
-                <div className='col-start-1 col-end-4 self-center justify-self-center mt-10'>
-                  <div className='flex justify-center items-center px-4 border-[1px] border-dashed md:h-[266px] md:w-[717px] p-10'>
-                    <p className=' text-black900/[.7] lg:text-3xl text-center'>
-                      ¡No tienes reservas canceladas desde hace mucho tiempo!
-                    </p>
-                  </div>
-                </div>
-              )}
-            </>
-          ) : null}
+                ) : null
+              )
+            : null}
         </div>
       ) : (
-        <div className='flex justify-center items-center'>
-          <div className='flex justify-center items-center px-4 border-[1px] border-dashed h-[266px] w-[717px]'>
-            <p className=' text-black900/[.7] lg:text-3xl text-center'>
+        <div className="flex justify-center items-center">
+          <div className="flex justify-center items-center px-4 border-[1px] border-dashed h-[266px] w-[717px]">
+            <p className=" text-black900/[.7] lg:text-3xl text-center">
               ¡Aún no tienes reservaciones en tu alojamiento!
             </p>
           </div>
--- components/OwnerAccomodationReservationDetails/OwnerAccomodationReservationDetails.tsx
+++ components/OwnerAccomodationReservationDetails/OwnerAccomodationReservationDetails.tsx
@@ -1,9 +1,9 @@
-import React, { FC, useEffect, useState } from 'react';
-import axios from 'axios';
-import { ActiveReservations } from './ActiveReservations';
-import moment from 'moment';
-import { RangeOfDates } from './RangeOfDates';
-import { CancelledRangeOfDates } from './CancelledRangeOfDates';
+import React, { FC, useEffect, useState } from "react";
+import axios from "axios";
+import { ActiveReservations } from "./ActiveReservations";
+import moment from "moment";
+import { RangeOfDates } from "./RangeOfDates";
+import { CancelledRangeOfDates } from "./CancelledRangeOfDates";
 
 interface ReservationCalendarProps {
   start: string;
@@ -26,25 +26,25 @@ export const OwnerAccomodationReservationDetails: FC<{ id: string }> = ({
 }) => {
   const [bookingById, setBookingById] = useState<IBookingId[]>([
     {
-      id: '',
-      travellerName: '',
-      accommodationName: '',
-      status: '',
+      id: "",
+      travellerName: "",
+      accommodationName: "",
+      status: "",
       occupiedPeriod: {
-        startDate: '',
-        endDate: '',
+        startDate: "",
+        endDate: "",
       },
     },
   ]);
   const [beforeBooking, setBeforeBooking] = useState<IBookingId[]>([
     {
-      id: '',
-      travellerName: '',
-      accommodationName: '',
-      status: '',
+      id: "",
+      travellerName: "",
+      accommodationName: "",
+      status: "",
       occupiedPeriod: {
-        startDate: '',
-        endDate: '',
+        startDate: "",
+        endDate: "",
       },
     },
   ]);
@@ -52,11 +52,11 @@ export const OwnerAccomodationReservationDetails: FC<{ id: string }> = ({
     ReservationCalendarProps[]
   >([
     {
-      start: '',
-      end: '',
+      start: "",
+      end: "",
     },
   ]);
-  const [activeTab, setActiveTab] = useState('active');
+  const [activeTab, setActiveTab] = useState("active");
 
   useEffect(() => {
     const accomodationBlockDay = async (id: string) => {
@@ -65,8 +65,8 @@ export const OwnerAccomodationReservationDetails: FC<{ id: string }> = ({
           `https://api.avaibook.com/api/owner/accommodations/${id}/calendar/`,
           {
             headers: {
-              'Content-Type': 'application/json',
-              'X-AUTH-TOKEN': process.env.AVAIBOOK_API_TOKEN,
+              "Content-Type": "application/json",
+              "X-AUTH-TOKEN": process.env.AVAIBOOK_API_TOKEN,
             },
           }
         );
@@ -86,8 +86,8 @@ export const OwnerAccomodationReservationDetails: FC<{ id: string }> = ({
             `https://api.avaibook.com/api/owner/bookings/${id}/`,
             {
               headers: {
-                'Content-Type': 'application/json',
-                'X-AUTH-TOKEN': process.env.AVAIBOOK_API_TOKEN,
+                "Content-Type": "application/json",
+                "X-AUTH-TOKEN": process.env.AVAIBOOK_API_TOKEN,
               },
             }
           );
@@ -110,18 +110,18 @@ export const OwnerAccomodationReservationDetails: FC<{ id: string }> = ({
 
     const getBookingdate90DaysAgo = async (id: string) => {
       let currentDate = moment();
-      let date90DaysAgo = moment().subtract(90, 'days');
+      let date90DaysAgo = moment().subtract(90, "days");
 
       try {
         if (id) {
           const res = await axios.get(
             `https://api.avaibook.com/api/owner/accommodations/${id}/calendar/?startDate=${date90DaysAgo.format(
-              'YYYY-MM-DD'
-            )}&endDate=${currentDate.format('YYYY-MM-DD')}`,
+              "YYYY-MM-DD"
+            )}&endDate=${currentDate.format("YYYY-MM-DD")}`,
             {
               headers: {
-                'Content-Type': 'application/json',
-                'X-AUTH-TOKEN': process.env.AVAIBOOK_API_TOKEN,
+                "Content-Type": "application/json",
+                "X-AUTH-TOKEN": process.env.AVAIBOOK_API_TOKEN,
               },
             }
           );
@@ -131,8 +131,8 @@ export const OwnerAccomodationReservationDetails: FC<{ id: string }> = ({
               `https://api.avaibook.com/api/owner/bookings/${booking.booking}/`,
               {
                 headers: {
-                  'Content-Type': 'application/json',
-                  'X-AUTH-TOKEN': process.env.AVAIBOOK_API_TOKEN,
+                  "Content-Type": "application/json",
+                  "X-AUTH-TOKEN": process.env.AVAIBOOK_API_TOKEN,
                 },
               }
             );
@@ -150,7 +150,7 @@ export const OwnerAccomodationReservationDetails: FC<{ id: string }> = ({
           });
         }
       } catch (error) {
-        console.error('Error fetching bookings:', error);
+        console.error("Error fetching bookings:", error);
       }
     };
 
@@ -163,26 +163,19 @@ export const OwnerAccomodationReservationDetails: FC<{ id: string }> = ({
     });
 
     getBookingdate90DaysAgo(id);
-  }, [accomodationDayBlock]);
-
-  const bookingByIdSlice = bookingById.slice(1);
+  }, [accomodationDayBlock, id]);
 
   const renderTabContent = () => {
     switch (activeTab) {
-      case 'active':
+      case "active":
+        return <ActiveReservations bookings={bookingById} status="CONFIRMED" />;
+      case "past":
         return (
-          <ActiveReservations bookings={bookingByIdSlice} status='CONFIRMED' />
+          <ActiveReservations bookings={beforeBooking} status="CONFIRMED" />
         );
-      case 'past':
-        return (
-          <ActiveReservations
-            bookings={beforeBooking.slice(1)}
-            status='CONFIRMED'
-          />
-        );
-      case 'canceled':
+      case "canceled":
         return <CancelledRangeOfDates id={id} />;
-      case 'rangeOfDates':
+      case "rangeOfDates":
         return <RangeOfDates id={id} />;
       default:
         return null;
@@ -190,45 +183,45 @@ export const OwnerAccomodationReservationDetails: FC<{ id: string }> = ({
   };
 
   return (
-    <div className='px-8 space-y-12 mb-24'>
-      <div className='flex flex-col md:flex-row justify-center items-center md:justify-between md:items-end border-b-[1px]'>
-        <p className=' text-black900/[.7]  mt-10 text-2xl text-center md:text-left md:text-3xl lg:mt-16 lg:text-4xl'>
+    <div className="px-8 space-y-12 mb-24">
+      <div className="flex flex-col md:flex-row justify-center items-center md:justify-between md:items-end border-b-[1px]">
+        <p className=" text-black900/[.7]  mt-10 text-2xl text-center md:text-left md:text-3xl lg:mt-16 lg:text-4xl">
           Reservas en mi alojamiento
         </p>
 
-        <ul className='flex flex-col space-y-3 md:space-y-0 md:flex-row justify-center items-center md:space-x-10 mt-8  lg:text-lg'>
+        <ul className="flex flex-col space-y-3 md:space-y-0 md:flex-row justify-center items-center md:space-x-10 mt-8  lg:text-lg">
           <li
             className={`cursor-pointer ${
-              activeTab === 'active' ? 'text-p600' : ''
+              activeTab === "active" ? "text-p600" : ""
             }`}
-            onClick={() => setActiveTab('active')}
+            onClick={() => setActiveTab("active")}
           >
             Activas
           </li>
 
           <li
             className={`cursor-pointer ${
-              activeTab === 'past' ? 'text-p600' : ''
+              activeTab === "past" ? "text-p600" : ""
             }`}
-            onClick={() => setActiveTab('past')}
+            onClick={() => setActiveTab("past")}
           >
             Pasadas
           </li>
 
           <li
             className={`cursor-pointer ${
-              activeTab === 'canceled' ? 'text-p600' : ''
+              activeTab === "canceled" ? "text-p600" : ""
             }`}
-            onClick={() => setActiveTab('canceled')}
+            onClick={() => setActiveTab("canceled")}
           >
             Canceladas
           </li>
 
           <li
             className={`cursor-pointer ${
-              activeTab === 'rangeOfDates' ? 'text-p600' : ''
+              activeTab === "rangeOfDates" ? "text-p600" : ""
             }`}
-            onClick={() => setActiveTab('rangeOfDates')}
+            onClick={() => setActiveTab("rangeOfDates")}
           >
             Rango de fechas
           </li>
