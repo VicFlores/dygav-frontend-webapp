51768c874259fd4825b16dc5bb75747552388286
Fernando Aguilar <feraguilar6985@gmail.com>
2024-03-10 18:20:33 -0600

    feat(cors):enable cors for all domains

--- next.config.js
+++ next.config.js
@@ -6,7 +6,20 @@ const nextConfig = {
   },
   env: {
     AVAIBOOK_API_TOKEN: 'e8f0d39c83dff242a8e47744d1d0fc5f76e4e2363752f6fe117f7d7e2924144d'
-  }
+  },
+  // Enable cors for all domains
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, OPTIONS, POST' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-Requested-With, Content-Type, Accept, Origin, Authorization' },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
