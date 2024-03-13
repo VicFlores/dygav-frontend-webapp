083a81a073ab8702aa6fd038b87b5bfa38a4d3ad
VicFlores11 <vicflores2211@gmail.com>
2024-03-13 14:52:07 -0600

    refactor:[CHANGE] uncomment email funcionallity

--- components/BlockCalendarDaysForm/BlockCalendarDaysForm.tsx
+++ components/BlockCalendarDaysForm/BlockCalendarDaysForm.tsx
@@ -1,9 +1,9 @@
-import { axiosConfig } from "@/utils";
-import axios from "axios";
-import { useSession } from "next-auth/react";
-import { FC, useEffect, useState } from "react";
-import { useForm, SubmitHandler } from "react-hook-form";
-import moment from "moment";
+import { axiosConfig } from '@/utils';
+import axios from 'axios';
+import { useSession } from 'next-auth/react';
+import { FC, useEffect, useState } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import moment from 'moment';
 
 type FormValues = {
   startDate: string;
@@ -24,12 +24,12 @@ type Props = {
 };
 
 const BlockCalendarDaysForm: FC<Props> = ({ setlistenBlockDate, id }) => {
-  const [error, setError] = useState("");
+  const [error, setError] = useState('');
   const { register, handleSubmit } = useForm<FormValues>();
   const { data: session } = useSession();
   const [findAccomodationById, setfindAccomodationById] = useState<{
     name: string;
-  }>({ name: "" });
+  }>({ name: '' });
 
   useEffect(() => {
     const findAccomodation = async () => {
@@ -37,8 +37,8 @@ const BlockCalendarDaysForm: FC<Props> = ({ setlistenBlockDate, id }) => {
         `https://api.avaibook.com/api/owner/accommodations/${id}/`,
         {
           headers: {
-            "Content-Type": "application/json",
-            "X-AUTH-TOKEN": process.env.AVAIBOOK_API_TOKEN,
+            'Content-Type': 'application/json',
+            'X-AUTH-TOKEN': process.env.AVAIBOOK_API_TOKEN,
           },
         }
       );
@@ -61,68 +61,68 @@ const BlockCalendarDaysForm: FC<Props> = ({ setlistenBlockDate, id }) => {
       },
       {
         headers: {
-          "Content-Type": "application/json",
-          "X-AUTH-TOKEN": process.env.AVAIBOOK_API_TOKEN,
+          'Content-Type': 'application/json',
+          'X-AUTH-TOKEN': process.env.AVAIBOOK_API_TOKEN,
         },
       }
     );
 
     setlistenBlockDate(res.data);
 
-    // await axiosConfig.post("/api/mailing/blockDays", {
-    //   fullname: "Jose Llaneza",
-    //   owner: session?.user?.fullname,
-    //   email: session?.user?.email,
-    //   startDate: moment(startDate).format("dddd D [de] MMMM [de] YYYY"),
-    //   endDate: moment(endDate).format("dddd D [de] MMMM [de] YYYY"),
-    //   accomodation: findAccomodationById.name,
-    // });
+    await axiosConfig.post('/api/mailing/blockDays', {
+      fullname: 'Jose Llaneza',
+      owner: session?.user?.fullname,
+      email: session?.user?.email,
+      startDate: moment(startDate).format('dddd D [de] MMMM [de] YYYY'),
+      endDate: moment(endDate).format('dddd D [de] MMMM [de] YYYY'),
+      accomodation: findAccomodationById.name,
+    });
 
-    setError("Dias bloqueados con exito");
+    setError('Dias bloqueados con exito');
   };
 
   return (
-    <div className="flex justify-center items-center mt-10">
+    <div className='flex justify-center items-center mt-10'>
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className="grid grid-cols-1 md:grid-cols-2 mt-4 bg-gray300/75 rounded-xl p-12 lg:p-10 lg:gap-10"
+        className='grid grid-cols-1 md:grid-cols-2 mt-4 bg-gray300/75 rounded-xl p-12 lg:p-10 lg:gap-10'
       >
-        <div className="mb-8 text-center lg:text-start lg:mb-4">
+        <div className='mb-8 text-center lg:text-start lg:mb-4'>
           <label
-            className="text-gray-700 font-semibold block mb-2"
-            htmlFor="name"
+            className='text-gray-700 font-semibold block mb-2'
+            htmlFor='name'
           >
             Fecha incio de bloqueo
           </label>
           <input
-            className="border border-gray-400 p-2 w-full rounded-lg text-center lg:text-start"
-            type="date"
-            {...register("startDate")}
+            className='border border-gray-400 p-2 w-full rounded-lg text-center lg:text-start'
+            type='date'
+            {...register('startDate')}
           />
         </div>
 
-        <div className="mb-8 text-center lg:text-start lg:mb-4">
+        <div className='mb-8 text-center lg:text-start lg:mb-4'>
           <label
-            className="text-gray-700 font-semibold block mb-2"
-            htmlFor="name"
+            className='text-gray-700 font-semibold block mb-2'
+            htmlFor='name'
           >
             Fecha fin de bloqueo
           </label>
           <input
-            className="border border-gray-400 p-2 w-full rounded-lg text-center lg:text-start"
-            type="date"
-            {...register("endDate")}
+            className='border border-gray-400 p-2 w-full rounded-lg text-center lg:text-start'
+            type='date'
+            {...register('endDate')}
           />
         </div>
 
         <button
-          className="text-sm md:text-base bg-p600 self-center justify-self-center text-white py-2 px-10 rounded-lg hover:bg-blue-600 lg:col-start-1 lg:col-end-3"
-          type="submit"
+          className='text-sm md:text-base bg-p600 self-center justify-self-center text-white py-2 px-10 rounded-lg hover:bg-blue-600 lg:col-start-1 lg:col-end-3'
+          type='submit'
         >
           Bloquear Fechas
         </button>
 
-        <span className="self-center justify-self-center lg:col-start-1 lg:col-end-3">
+        <span className='self-center justify-self-center lg:col-start-1 lg:col-end-3'>
           {error}
         </span>
       </form>
