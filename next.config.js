35f0977740a7f74a8d42c8d8d594f0e0861794f8
VicFlores11 <vicflores2211@gmail.com>
2024-03-18 15:12:20 -0600

    refactor:[CHANGE] distribution code

--- next.config.js
+++ next.config.js
@@ -2,10 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['res.cloudinary.com', 'lh3.googleusercontent.com', 'www.avaibook.com', 'dygav-wordpress.app.bigital.es']
+    domains: [
+      'res.cloudinary.com',
+      'lh3.googleusercontent.com',
+      'www.avaibook.com',
+      'dygav-wordpress.app.bigital.es',
+    ],
   },
   env: {
-    AVAIBOOK_API_TOKEN: 'e8f0d39c83dff242a8e47744d1d0fc5f76e4e2363752f6fe117f7d7e2924144d'
+    AVAIBOOK_API_TOKEN:
+      'e8f0d39c83dff242a8e47744d1d0fc5f76e4e2363752f6fe117f7d7e2924144d',
   },
   // Enable cors for all domains
   async headers() {
@@ -15,11 +21,15 @@ const nextConfig = {
         headers: [
           { key: 'Access-Control-Allow-Origin', value: '*' },
           { key: 'Access-Control-Allow-Methods', value: 'GET, OPTIONS, POST' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-Requested-With, Content-Type, Accept, Origin, Authorization' },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-Requested-With, Content-Type, Accept, Origin, Authorization',
+          },
         ],
       },
-    ]
+    ];
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
