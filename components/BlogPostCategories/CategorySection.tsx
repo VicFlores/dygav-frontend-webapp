536b15bf662526d684b4367ab7ac7ee6ac06d27f
VicFlores11 <vicflores2211@gmail.com>
2024-03-18 15:17:57 -0600

    fix:[ADD] session user id for google users

--- components/BlogPostCategories/CategorySection.tsx
+++ components/BlogPostCategories/CategorySection.tsx
@@ -1,13 +1,13 @@
-import { BlogPost } from "@/types";
-import { axiosConfig } from "@/utils";
-import moment from "moment";
-import { useSession } from "next-auth/react";
-import Image from "next/legacy/image";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import React, { FC, useEffect, useRef, useState } from "react";
-import { AiOutlineArrowRight, AiOutlineArrowLeft } from "react-icons/ai";
-import { FaHeart, FaRegHeart } from "react-icons/fa6";
+import { BlogPost } from '@/types';
+import { axiosConfig } from '@/utils';
+import moment from 'moment';
+import { useSession } from 'next-auth/react';
+import Image from 'next/legacy/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React, { FC, useEffect, useRef, useState } from 'react';
+import { AiOutlineArrowRight, AiOutlineArrowLeft } from 'react-icons/ai';
+import { FaHeart, FaRegHeart } from 'react-icons/fa6';
 
 type BlogPostWithSubCategory = BlogPost & {
   subCategoryName: string | null;
@@ -21,7 +21,7 @@ type CategorySectionProps = {
 };
 
 const defaultImage =
-  "https://res.cloudinary.com/vicflores11/image/upload/v1703948850/Dygav/undraw_publish_article_re_3x8h_w41fes.svg";
+  'https://res.cloudinary.com/vicflores11/image/upload/v1703948850/Dygav/undraw_publish_article_re_3x8h_w41fes.svg';
 
 export const CategorySection: FC<CategorySectionProps> = ({
   title,
@@ -40,7 +40,9 @@ export const CategorySection: FC<CategorySectionProps> = ({
       try {
         if (session && session.user) {
           const res = await axiosConfig.get(
-            `/api/favorites/findFavBlogPostsByUserId?userId=${session.user._id}`
+            `/api/favorites/findFavBlogPostsByUserId?userId=${
+              session.user._id || session.user.id
+            }`
           );
 
           setFavExist(res.data);
@@ -66,12 +68,12 @@ export const CategorySection: FC<CategorySectionProps> = ({
   };
 
   const formatedTtitle =
-    title === "Consejos y Recursos para Viajeros"
-      ? "Guías de viaje"
-      : title === "Trámites y Legislación para Alquileres Vacacionales"
-      ? "Trámites para licencias turísticas"
-      : title === "Tendencias y Evolución del Alquiler Vacacional"
-      ? "Tendencias sobre alquiler vacacional"
+    title === 'Consejos y Recursos para Viajeros'
+      ? 'Guías de viaje'
+      : title === 'Trámites y Legislación para Alquileres Vacacionales'
+      ? 'Trámites para licencias turísticas'
+      : title === 'Tendencias y Evolución del Alquiler Vacacional'
+      ? 'Tendencias sobre alquiler vacacional'
       : title;
 
   const handleFavAdd = (post: any) => {
@@ -79,8 +81,8 @@ export const CategorySection: FC<CategorySectionProps> = ({
       const addFav = async () => {
         try {
           if (session.user) {
-            await axiosConfig.post("/api/favorites/favoriteBlogPosts", {
-              userId: session.user._id,
+            await axiosConfig.post('/api/favorites/favoriteBlogPosts', {
+              userId: session.user._id || session.user.id,
               blogPostId: post.id,
             });
 
@@ -93,7 +95,7 @@ export const CategorySection: FC<CategorySectionProps> = ({
 
       addFav();
     } else {
-      return router.push("/login");
+      return router.push('/login');
     }
   };
 
@@ -104,7 +106,7 @@ export const CategorySection: FC<CategorySectionProps> = ({
           if (session.user) {
             await axiosConfig.delete(`/api/favorites/favoriteBlogPosts`, {
               data: {
-                userId: session.user._id,
+                userId: session.user._id || session.user.id,
                 blogPostId: post.id,
               },
             });
@@ -122,36 +124,36 @@ export const CategorySection: FC<CategorySectionProps> = ({
 
       setfavIsChanged(!favIsChanged);
     } else {
-      return router.push("/login");
+      return router.push('/login');
     }
   };
 
   return (
     <>
-      <h2 className="mt-0 md:mt-10 text-2xl md:text-4xl text-center pb-4 font-semibold border-b-4 border-b-p600 w-auto md:px-10">
+      <h2 className='mt-0 md:mt-10 text-2xl md:text-4xl text-center pb-4 font-semibold border-b-4 border-b-p600 w-auto md:px-10'>
         {title}
       </h2>
 
-      <h3 className="mt-6 text-base md:text-lg lg:text-xl text-center text-[#45474B]">
+      <h3 className='mt-6 text-base md:text-lg lg:text-xl text-center text-[#45474B]'>
         {subtitle}
       </h3>
 
-      <p className="mt-10 text-base md:text-lg text-justify md:px-20">
+      <p className='mt-10 text-base md:text-lg text-justify md:px-20'>
         {information}
       </p>
 
-      <div className="mt-20 w-full md:px-10 mb-16">
-        <div className="flex items-center justify-center md:space-x-14 overflow-auto overflow-x-visible pb-8">
+      <div className='mt-20 w-full md:px-10 mb-16'>
+        <div className='flex items-center justify-center md:space-x-14 overflow-auto overflow-x-visible pb-8'>
           <button
             onClick={scrollLeft}
-            className="hidden lg:flex p-2 relative z-10 text-white bg-p600/80 ml-3 -bottom-36"
+            className='hidden lg:flex p-2 relative z-10 text-white bg-p600/80 ml-3 -bottom-36'
           >
             <AiOutlineArrowLeft />
           </button>
 
           <div
             ref={scrollContainer}
-            className="w-auto h-auto items-center pb-8 overflow-x-auto overscroll-x-contain flex space-x-6 overflow-y-hidden scrollbar"
+            className='w-auto h-auto items-center pb-8 overflow-x-auto overscroll-x-contain flex space-x-6 overflow-y-hidden scrollbar'
           >
             {posts.filter((post) => {
               if (post.subCategoryName) {
@@ -189,53 +191,53 @@ export const CategorySection: FC<CategorySectionProps> = ({
                   return (
                     <div key={post.id}>
                       <div
-                        className="relative flex flex-col justify-end min-w-[300px] min-h-[350px]  md:min-w-[494px] md:h-[296px] rounded-xl px-6 pb-6 space-y-2 text-white"
+                        className='relative flex flex-col justify-end min-w-[300px] min-h-[350px]  md:min-w-[494px] md:h-[296px] rounded-xl px-6 pb-6 space-y-2 text-white'
                         style={{
                           backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.5)), url(${post.yoast_head_json.og_image[0].url})`,
-                          backgroundSize: "cover",
-                          backgroundPosition: "center",
-                          boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.25)",
+                          backgroundSize: 'cover',
+                          backgroundPosition: 'center',
+                          boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.25)',
                         }}
                       >
-                        <div className="absolute top-0 right-0 z-10 p-6 text-[30px]">
+                        <div className='absolute top-0 right-0 z-10 p-6 text-[30px]'>
                           {disableFav ? (
                             <FaHeart
                               onClick={() => handleFavRemove(post)}
-                              className="text-p600"
+                              className='text-p600'
                             />
                           ) : (
                             <FaRegHeart
                               onClick={() => handleFavAdd(post)}
-                              className="text-white"
+                              className='text-white'
                             />
                           )}
                         </div>
 
-                        <p className="underline">
+                        <p className='underline'>
                           <Link href={`/post/${post.slug}`}>
                             {post.title.rendered}
                           </Link>
                         </p>
-                        <p>{moment(post.date).format("D MMMM, YYYY")}</p>
+                        <p>{moment(post.date).format('D MMMM, YYYY')}</p>
                       </div>
                     </div>
                   );
                 })
             ) : (
               <div>
-                <div className="flex flex-col justify-center items-center min-w-[300px] min-h-[350px]  md:min-w-[494px] md:h-[296px] border-2 border-p600 rounded-xl text-p600">
-                  <figure className="relative w-52 h-52 md:w-60 md:h-60 m-auto">
+                <div className='flex flex-col justify-center items-center min-w-[300px] min-h-[350px]  md:min-w-[494px] md:h-[296px] border-2 border-p600 rounded-xl text-p600'>
+                  <figure className='relative w-52 h-52 md:w-60 md:h-60 m-auto'>
                     <Image
                       src={defaultImage}
-                      layout="fill"
+                      layout='fill'
                       priority
                       alt={
-                        " Estamos trabajando para entregarte nuevos articulos"
+                        ' Estamos trabajando para entregarte nuevos articulos'
                       }
                     />
                   </figure>
 
-                  <p className="pb-6 text-center px-4">
+                  <p className='pb-6 text-center px-4'>
                     Estamos trabajando para entregarte nuevos articulos
                   </p>
                 </div>
@@ -245,7 +247,7 @@ export const CategorySection: FC<CategorySectionProps> = ({
 
           <button
             onClick={scrollRight}
-            className="hidden lg:flex p-2 relative z-10 text-white bg-p600/80 ml-3 -bottom-36"
+            className='hidden lg:flex p-2 relative z-10 text-white bg-p600/80 ml-3 -bottom-36'
           >
             <AiOutlineArrowRight />
           </button>
