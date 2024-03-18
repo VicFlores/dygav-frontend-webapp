6ed10d59fb622c652e3c9406a47a69545fd2eef7
VicFlores11 <vicflores2211@gmail.com>
2024-03-18 17:17:57 -0600

    feat:[REMOVE] check reservation listener

--- components/NewAccomodation/NewAccomodation.tsx
+++ components/NewAccomodation/NewAccomodation.tsx
@@ -1,16 +1,6 @@
-import { useEffect } from 'react';
 import NewAccomodationForm from './NewAccomodationForm';
-import { axiosConfig } from '@/utils';
 
 export const NewAccomodation = () => {
-  useEffect(() => {
-    const startReservationsService = async () => {
-      await axiosConfig.get('/api/accomodations/checkingReservations');
-    };
-
-    startReservationsService();
-  }, []);
-
   return (
     <section className='px-8 space-y-14 mb-48'>
       <p className=' text-black900/[.7] border-b-[1px] mt-10 text-2xl text-center md:text-left md:text-3xl lg:mt-16 lg:text-4xl'>
