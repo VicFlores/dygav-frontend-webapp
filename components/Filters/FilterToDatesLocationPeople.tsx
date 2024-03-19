22004407e88c9edba73efa02c713fbb8876d41bd
VicFlores11 <vicflores2211@gmail.com>
2024-03-19 16:40:03 -0600

    styles:[CHANGE] styles of calendar

--- components/Filters/FilterToDatesLocationPeople.tsx
+++ components/Filters/FilterToDatesLocationPeople.tsx
@@ -1,24 +1,29 @@
-import { FaLocationDot } from "react-icons/fa6";
-import { BsCalendar2DateFill, BsPeopleFill } from "react-icons/bs";
-import { AiOutlineCheckCircle } from "react-icons/ai";
-import { SubmitHandler, useForm } from "react-hook-form";
-import DatePicker from "react-datepicker";
-import { registerLocale } from "react-datepicker";
-import { es } from "date-fns/locale";
-import { useRouter } from "next/router";
-import { useState } from "react";
-import { addMonths } from "date-fns";
-import moment from "moment";
-import "react-datepicker/dist/react-datepicker.css";
-registerLocale("es", es);
+import { FaLocationDot } from 'react-icons/fa6';
+import { BsCalendar2DateFill, BsPeopleFill } from 'react-icons/bs';
+import { AiOutlineCheckCircle } from 'react-icons/ai';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import DatePicker from 'react-datepicker';
+import { registerLocale } from 'react-datepicker';
+import { es } from 'date-fns/locale';
+import { useRouter } from 'next/router';
+import { forwardRef, useState } from 'react';
+import { addMonths } from 'date-fns';
+import moment from 'moment';
+import 'react-datepicker/dist/react-datepicker.css';
+registerLocale('es', es);
 
 interface FormData {
   ubicacion: string;
   huespedes: number;
 }
 
+interface ExampleCustomInputProps {
+  value?: string;
+  onClick?: () => void;
+}
+
 export const FilterToDatesLocationPeople = () => {
-  const [selectedOption, setSelectedOption] = useState("default");
+  const [selectedOption, setSelectedOption] = useState('default');
   const [dateRange, setDateRange] = useState<[Date | null, Date | null]>([
     null,
     null,
@@ -29,24 +34,24 @@ export const FilterToDatesLocationPeople = () => {
 
   const capitalizeFirstLetterOfEachWord = (string: string) => {
     return string
-      .split(" ")
+      .split(' ')
       .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-      .join(" ");
+      .join(' ');
   };
 
   const onSubmit: SubmitHandler<FormData> = ({ ubicacion, huespedes }) => {
     const capitalizedUbicacion = capitalizeFirstLetterOfEachWord(ubicacion);
 
     if (!startDate || !endDate) {
-      return alert("Por favor, selecciona las fechas de reserva");
+      return alert('Por favor, selecciona las fechas de reserva');
     }
 
     router.push({
       pathname: `/apartamentos-turisticos/${capitalizedUbicacion}`,
       query: {
         ubicacion: capitalizedUbicacion,
-        checkin: moment(startDate).format("YYYY-MM-DD"),
-        checkout: moment(endDate).format("YYYY-MM-DD"),
+        checkin: moment(startDate).format('YYYY-MM-DD'),
+        checkout: moment(endDate).format('YYYY-MM-DD'),
         huespedes,
       },
     });
@@ -55,35 +60,35 @@ export const FilterToDatesLocationPeople = () => {
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
-      className="grid grid-cols-1 lg:grid-cols-4 lg:gap-0 md:gap-8 gap-6 py-6 mt-16 bg-p600/50"
+      className='grid grid-cols-1 lg:grid-cols-4 lg:gap-0 md:gap-8 gap-6 py-6 mt-16 bg-p600/50'
     >
-      <div className="text-center self-center justify-self-center">
-        <p className="pb-2 font-semibold">Escoge tu ciudad</p>
-        <label className="relative">
-          <FaLocationDot className="w-5 h-5 absolute top-1/2 -translate-y-1/2 left-3 text-black900" />
+      <div className='text-center self-center justify-self-center'>
+        <p className='pb-2 font-semibold'>Escoge tu ciudad</p>
+        <label className='relative'>
+          <FaLocationDot className='w-5 h-5 absolute top-1/2 -translate-y-1/2 left-3 text-black900' />
           <select
-            {...register("ubicacion", { required: true })}
-            className="rounded-lg pl-10 pr-4 lg:w-full lg:h-11 md:w-96 w-full bg-transparent shadow appearance-none bg-white border border-white placeholder:text-black900 py-2 text-black900 leading-tight focus:outline-none focus:shadow-outline"
+            {...register('ubicacion', { required: true })}
+            className='rounded-lg pl-10 pr-4 lg:w-[275px] lg:h-11 md:w-96 w-[240px] bg-transparent shadow appearance-none bg-white border border-white placeholder:text-black900 py-2 text-black900 leading-tight focus:outline-none focus:shadow-outline'
             value={selectedOption}
             onChange={(e) => setSelectedOption(e.target.value)}
           >
-            <option value="default" disabled>
+            <option value='default' disabled>
               Ciudad
             </option>
-            <option value="torrevieja">Torrevieja</option>
-            <option value="madrid">Madrid</option>
-            <option value="puerto de mazarron">Puerto De Mazarron</option>
-            <option value="lo pagan">Lo Pagan</option>
-            <option value="finestrat">Finestrat</option>
+            <option value='torrevieja'>Torrevieja</option>
+            <option value='madrid'>Madrid</option>
+            <option value='puerto de mazarron'>Puerto De Mazarron</option>
+            <option value='lo pagan'>Lo Pagan</option>
+            <option value='finestrat'>Finestrat</option>
           </select>
         </label>
       </div>
 
-      <div className="text-center self-center justify-self-center">
-        <p className="pb-2 font-semibold">Checkin - Checkout</p>
+      <div className='text-center self-center justify-self-center'>
+        <p className='pb-2 font-semibold'>Checkin - Checkout</p>
 
-        <div className="relative">
-          <BsCalendar2DateFill className="w-5 h-5 absolute top-1/2 -translate-y-1/2 left-3 text-black900 z-10" />
+        <div className='relative'>
+          <BsCalendar2DateFill className='w-5 h-5 absolute top-1/2 -translate-y-1/2 left-3 text-black900 z-10' />
           <DatePicker
             selectsRange={true}
             startDate={startDate}
@@ -93,34 +98,35 @@ export const FilterToDatesLocationPeople = () => {
             onChange={(update) => {
               setDateRange(update);
             }}
-            className="rounded-lg pl-10 pr-8 lg:w-[275px] lg:h-11 md:w-96 w-[240px] bg-transparent shadow appearance-none bg-white border border-white placeholder:text-black900 py-2 text-black900 leading-tight focus:outline-none focus:shadow-outline"
-            placeholderText="Fechas de reserva"
+            placeholderText='Fechas de reserva'
             isClearable
             locale={es}
-            dateFormat={"dd/MM/yyyy"}
+            className='rounded-lg pl-10 pr-8 lg:w-[275px] lg:h-11 md:w-96 w-[240px] bg-transparent shadow appearance-none bg-white border border-white placeholder:text-black900 py-2 text-black900 leading-tight focus:outline-none focus:shadow-outline'
+            dateFormat={'dd/MM/yyyy'}
+            calendarClassName='lg:w-[275px] lg:h-11 md:w-96 w-[240px]'
           />
         </div>
       </div>
 
-      <div className="text-center self-center justify-self-center">
-        <p className="pb-2 font-semibold">Número de Viajeros</p>
-        <label className="relative">
-          <BsPeopleFill className="w-5 h-5 absolute top-1/2 -translate-y-1/2 left-3 text-black900" />
+      <div className='text-center self-center justify-self-center'>
+        <p className='pb-2 font-semibold'>Número de Viajeros</p>
+        <label className='relative'>
+          <BsPeopleFill className='w-5 h-5 absolute top-1/2 -translate-y-1/2 left-3 text-black900' />
           <input
-            {...register("huespedes", { required: true })}
-            type="number"
-            placeholder="Huéspedes"
-            className="rounded-lg pl-10 pr-4 lg:w-48 lg:h-11 md:w-96 w-[215px] bg-transparent shadow appearance-none bg-white border border-white placeholder:text-black900 py-2 text-black900 leading-tight focus:outline-none focus:shadow-outline"
+            {...register('huespedes', { required: true })}
+            type='number'
+            placeholder='Huéspedes'
+            className='rounded-lg pl-10 pr-4 lg:w-[275px] lg:h-11 md:w-96 w-[240px] bg-transparent shadow appearance-none bg-white border border-white placeholder:text-black900 py-2 text-black900 leading-tight focus:outline-none focus:shadow-outline'
           />
         </label>
       </div>
 
-      <div className="self-end justify-self-center relative">
-        <div className="relative">
-          <AiOutlineCheckCircle className="w-5 h-5 absolute top-1/2 -translate-y-1/2 right-7" />
+      <div className='self-end justify-self-center relative'>
+        <div className='relative'>
+          <AiOutlineCheckCircle className='w-5 h-5 absolute top-1/2 -translate-y-1/2 right-7' />
           <button
-            type="submit"
-            className="bg-p600 hover:bg-p800 py-2 px-4 w-[180px] text-left text-white justify-self-center self-center"
+            type='submit'
+            className='bg-p600 hover:bg-p800 py-2 px-4 w-[180px] text-left text-white justify-self-center self-center'
           >
             Buscar ahora
           </button>
