7759f353c7c2f51aeadec7a2a51c7c3daf72659e
VicFlores11 <vicflores2211@gmail.com>
2024-04-15 13:48:57 -0600

    feat:[ADD] cally calendar and conf

--- nextauth.d.ts
+++ nextauth.d.ts
@@ -21,3 +21,27 @@ declare module 'next-auth/jwt' {
     role?: Role;
   }
 }
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'calendar-range': React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement> & {
+          firstDayOfWeek?: number;
+          locale?: string;
+          value?: string;
+        },
+        HTMLElement
+      >;
+      'calendar-month': React.DetailedHTMLProps<
+        React.HTMLAttributes<
+          HTMLElement & {
+            offset?: string;
+          }
+        >,
+        HTMLElement
+      >;
+      // Add other Atomico components as needed
+    }
+  }
+}
