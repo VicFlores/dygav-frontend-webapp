ba7b53a18551583428db776a3205c37e2d0429a1
VicFlores11 <vicflores2211@gmail.com>
2024-04-26 17:52:52 -0600

    styles:[CHANGE]: margin top and bottom

--- components/Licenses/MadridInfo.tsx
+++ components/Licenses/MadridInfo.tsx
@@ -18,7 +18,7 @@ const images = [
 
 export const MadridInfo = () => {
   return (
-    <section className='mt-24 mb-16 px-6 md:px-12 lg:px-32 flex flex-col items-center'>
+    <section className='mt-24 md:mb-16 px-6 md:px-12 lg:px-32 flex flex-col items-center'>
       <h1 className='text-center text-2xl md:text-3xl pb-4 mb-6 font-semibold border-b-4 border-b-p600 w-auto md:px-10'>
         Licencias turísticas en Madrid
       </h1>
