f37ebeed69664d1921b84d7f196f046aeacfd856
VicFlores11 <vicflores2211@gmail.com>
2024-12-06 19:14:01 -0600

    feat:[ADD]  resolve merge

--- components/AdminDashboard/AdminDashboard.tsx
+++ components/AdminDashboard/AdminDashboard.tsx
@@ -66,7 +66,7 @@ export const AdminDashboard = () => {
               onClick={onClose}
               className='bg-[#1B3C73] py-2 px-6 md:py-2 md:px-8 rounded-xl mt-6'
             >
-              No
+              No.
             </button>
           </div>
         );
